e584ccf64b0f3aefc7a22591de18847ddf9de4b4
jkage <jaredkage@gmail.com>
2012-03-12 17:17:19 +0300

    original database file

--- app/Config/database.php
+++ app/Config/database.php
@@ -2,7 +2,7 @@
 class DATABASE_CONFIG {
 
 	public $test = array(
-		'datasource' => 'Database/Mysql',
+		'datasource' => 'Database/Postgres',
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'caketest',
@@ -11,7 +11,7 @@ class DATABASE_CONFIG {
 		'encoding' => 'utf8'
 	);
 	public $default = array(
-		'datasource' => 'Database/Mysql',
+		'datasource' => 'Database/Postgres',
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'cake',
