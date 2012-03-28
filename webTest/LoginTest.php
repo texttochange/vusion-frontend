00b86152955077e618bb1137887af1d2cf0cb6b4
Olivier Vernin <overnin@gmail.com>
2012-03-28 12:15:01 +0300

    add webtest

---
+++ webTest/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+class Login extends PHPUnit_Extensions_SeleniumTestCase
+{
+
+
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl("http://localhost:81/users/login");
+  }
+
+
+  public function testLogin()
+  {
+  	  $this->open("/users/login");
+    // $this->assertTitleEquals('Example Web Page');
+    $this->type("id=UserEmail", "marcus@texttochange.com");
+    $this->type("id=UserPassword", "marcus");
+    $this->click("css=input[type=\"submit\"]");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("Login successful.", $this->getText("id=flashMessage"));
+  }
+
+  public function testProgramManagerCannotEditProgramSettings()
+  {
+    $this->open("/users/login");
+    $this->type("id=UserEmail", "maureen@texttochange.com");
+    $this->type("id=UserPassword", "maureen");
+    $this->click("css=input[type=\"submit\"]");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Home");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Program Settings");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue((bool)preg_match('/^[\s\S]*\/view$/',$this->getLocation()));
+  }
+
+
+}
