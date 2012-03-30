75a15e5ccd5930bd6f15bc4e81fe6eb1de4b8a37
jkage <jaredkage@gmail.com>
2012-03-30 15:42:41 +0300

    added password for disappear flash message UI test

--- webTest/FlashMessageDisappearTest.php
+++ webTest/FlashMessageDisappearTest.php
@@ -11,6 +11,7 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
   {
     $this->open("/users/login");
     $this->type("id=UserEmail", "marcus@texttochange.com");
+    $this->type("id=UserPassword", "marcus");
     $this->click("css=input[type=\"submit\"]");
     $this->waitForPageToLoad("30000");
     $this->assertTrue($this->isElementPresent("id=flashMessage"));
