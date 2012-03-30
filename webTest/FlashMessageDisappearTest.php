16d59afb2523a41d79f333ed50002c1a0370467b
jkage <jaredkage@gmail.com>
2012-03-30 11:46:06 +0300

    Added UI test for success flash message disappear action

---
+++ webTest/FlashMessageDisappearTest.php
@@ -0,0 +1,27 @@
+<?php
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl("http://localhost:81/");
+  }
+
+  public function testMyTestCase()
+  {
+    $this->open("/users/login");
+    $this->type("id=UserEmail", "marcus@texttochange.com");
+    $this->click("css=input[type=\"submit\"]");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("id=flashMessage"));
+    for ($second = 0; ; $second++) {
+        if ($second >= 60) $this->fail("timeout");
+        try {
+            if (!$this->isVisible("id=flashMessage")) break;
+        } catch (Exception $e) {}
+        sleep(1);
+    }
+
+  }
+}
+?>
