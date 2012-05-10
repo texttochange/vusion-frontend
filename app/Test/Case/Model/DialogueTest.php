108abac61e4e376dcd585b3149cbe89e75eabcab
Olivier Vernin <overnin@gmail.com>
2012-05-10 15:50:28 +0300

    removed Controller and Model of script

--- app/Test/Case/Model/DialogueTest.php
+++ app/Test/Case/Model/DialogueTest.php
@@ -97,14 +97,13 @@ class DialogueTestCase extends CakeTestCase
         $saveNewVersionOtherDialogue = $this->Dialogue->saveDialogue($data);
         $this->Dialogue->makeDraftActive($saveNewVersionOtherDialogue['Dialogue']['dialogue-id']);
         $activeAndDraft = $this->Dialogue->getActiveAndDraft();
-        $this->assertEquals($saveNewVersionOtherDialogue['Dialogue']['_id'], $activeAndDraft[1]['Active']['_id']);
+        $this->assertEquals($saveNewVersionOtherDialogue['Dialogue']['_id'], $activeAndDraft[1]['Active']['_id']."");
         
         /**reactivate the olderone*/
         $this->Dialogue->makeActive($saveActiveOtherDialogue['Dialogue']['_id']);
         $activeAndDraft = $this->Dialogue->getActiveAndDraft();
         $this->assertEquals($saveActiveOtherDialogue['Dialogue']['_id'], $activeAndDraft[1]['Active']['_id']);
        
-
     }
 
 
