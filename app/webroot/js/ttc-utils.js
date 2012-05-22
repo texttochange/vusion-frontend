9ded503547ea49656ff7a4ad516025799b9012d6
Olivier Vernin <overnin@gmail.com>
2012-05-22 13:07:11 +0300

    fixed bug localized_errors object has wrong spelling

--- app/webroot/js/ttc-utils.js
+++ app/webroot/js/ttc-utils.js
@@ -276,10 +276,10 @@ function requestHelp(elt, baseUrl, topic) {
 
 function vusionAjaxError(jqXHR, textStatus, errorThrown){
     if (textStatus == 'timeout') {
-    	    $('#flashMessage').show().text(localized_error['vusion_ajax_timeout_error']);
+    	    $('#flashMessage').show().text(localized_errors['vusion_ajax_timeout_error']);
          return;
     }
-    $('#flashMessage').show().text(localized_error['vusion_ajax_error']);
+    $('#flashMessage').show().text(localized_errors['vusion_ajax_error']);
 }
 
 
