dba2f5d161fda21fd21e7b1e651ff88069a3dfdd
jkage <jaredkage@gmail.com>
2012-05-24 11:58:27 +0300

    fixed bug in ProgramParticipantsController test because phone number must always start with +

--- app/Model/Participant.php
+++ app/Model/Participant.php
@@ -40,6 +40,10 @@ class Participant extends MongoModel
     
     public function beforeValidate()
     {
+         $this->data['Participant']['phone'] = preg_replace("/[^0-9\+]/", "",$this->data['Participant']['phone']);
+         $this->data['Participant']['phone'] = preg_replace("/^(0|00)/", "+",$this->data['Participant']['phone']);
+         if (!preg_match('/^\+[0-9]+/', $this->data['Participant']['phone']))
+             $this->data['Participant']['phone'] = "+".$this->data['Participant']['phone'];
          $this->data['Participant']['phone'] = (string) $this->data['Participant']['phone'];
          if (isset($this->data['Participant']['name']))
              $this->data['Participant']['name'] = str_replace("\n" , "", $this->data['Participant']['name']);
