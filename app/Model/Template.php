a8f25edeecddd130746ba3abae0cfa39bf1f46fe
Olivier Vernin <overnin@gmail.com>
2012-05-28 19:06:52 +0300

    fixed bug declare empty variable in the Template model

--- app/Model/Template.php
+++ app/Model/Template.php
@@ -41,6 +41,7 @@ class Template extends MongoModel
     public function getTemplateOptions($type)
     {
         $templates = $this->find('all', array('conditions' => array('Template.type-template' => $type)));
+        $option = array();
         foreach ($templates as $template) {
             $options[$template['Template']['_id']] = $template['Template']['name'];
         }
