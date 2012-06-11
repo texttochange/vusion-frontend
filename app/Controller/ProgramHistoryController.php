d4d3a6ba9b0853d050260146b1840332a1c6ad67
Olivier Vernin <overnin@gmail.com>
2012-06-11 17:54:18 +0200

    refactor date conversion to handle dateonly format

--- app/Controller/ProgramHistoryController.php
+++ app/Controller/ProgramHistoryController.php
@@ -97,13 +97,13 @@ class ProgramHistoryController extends AppController
             if (isset($this->params['url']['filter_status']))
                 $conditions['message-status'] = $this->params['url']['filter_status'];
             if (isset($this->params['url']['filter_from']) && !isset($this->params['url']['filter_to'])) 
-                $conditions['timestamp'] = array('$gt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_from'].' 00:00'));
+                $conditions['timestamp'] = array('$gt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_from']));
             if (isset($this->params['url']['filter_to']) && !isset($this->params['url']['filter_from']))
-                $conditions['timestamp'] = array('$lt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_to'].' 00:00'));
+                $conditions['timestamp'] = array('$lt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_to']));
             if (isset($this->params['url']['filter_from']) && isset($this->params['url']['filter_to']))
                 $conditions['timestamp'] = array(
-                    '$gt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_from'].' 00:00'),
-                    '$lt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_to'].' 00:00')
+                    '$gt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_from']),
+                    '$lt'=>$this->dialogueHelper->ConvertDateFormat($this->params['url']['filter_to'])
                 );
             if (isset($this->params['url']['filter_phone'])) {
                 $phoneNumbers = explode(",", str_replace(" ", "",$this->params['url']['filter_phone']));
