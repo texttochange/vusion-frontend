bc5a9df86ebd1b2c084bc7873ed975c1c4972491
jkage <jaredkage@gmail.com>
2012-07-06 12:50:25 +0300

    fixed bug validation of date not in the past now works properly

--- app/webroot/js/ttc-generic-program.js
+++ app/webroot/js/ttc-generic-program.js
@@ -664,7 +664,8 @@ function fromBackendToFrontEnd(type, object, submitCall) {
         "greaterThanOrEqualTo", 
         function(value, element, params) {    
             if (!/Invalid|NaN/.test(Date.parse(value))) {
-                if (Date.parse(value).compareTo(Date.now())>0)
+                //if (Date.parse(value).compareTo(Date.now())>0)
+                if (Date.parseExact(value, "dd/MM/yyyy HH:mm").compareTo(Date.parseExact(Date.now().toString("dd/MM/yyyy HH:mm"), "dd/MM/yyyy HH:mm"))>0)
                     return true;
                 return false;
             }
