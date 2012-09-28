44d46ca44e8e1ba44e1e2c41450fb3aa4e6fa8ca
Olivier Vernin <overnin@gmail.com>
2012-09-28 19:28:41 +0300

    fix the participant test

--- app/Test/Case/Model/ParticipantTest.php
+++ app/Test/Case/Model/ParticipantTest.php
@@ -1,4 +1,4 @@
-<?php 
+    <?php 
 App::uses('Participant', 'Model');
 App::uses('ProgramSetting', 'Model');
 App::uses('Dialogue', 'Model');
@@ -52,7 +52,7 @@ class ParticipantTestCase extends CakeTestCase
         $this->assertRegExp('/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})$/', $savedParticipant['Participant']['last-optin-date']);
         $this->assertTrue(is_array( $savedParticipant['Participant']['tags']));
         $this->assertTrue(is_array( $savedParticipant['Participant']['enrolled']));
-        $this->assertTrue(is_array( $savedParticipant['Participant']['profile']));
+        $this->assertTrue($savedParticipant['Participant']['profile'] == null);
     }
 
 
