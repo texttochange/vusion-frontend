b3b740743e938de8d2d287848306e6b152e7cea9
markphilip <markphi119@gmail.com>
2013-04-18 16:15:35 +0300

    send message content limit to 160chars per message

--- app/webroot/js/counter.js
+++ app/webroot/js/counter.js
@@ -2,13 +2,24 @@ function addCounter(){
     $.each(
         $("[name*='content']"),        
         function (key, elt){
-            if ($(elt).prev('span').length>0) {
-                return;
-            }
+        	/*if ($(elt).prev('span').length > 0) {
+        		return;
+        	}
+        	$(elt).keydown(function()){
+            if ($(elt).prev('span').length > 160) {
+                alert("hi");
+                $(elt).val() = $(elt).val().substring(0, 160);
+            	
+            }*/            
             $("<span style='float: right; padding-right: 13px;'> <span id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span> Characters</span>").insertBefore($(elt));
-            $(elt).keyup(function(){
-                    //alert("hi");
-                    $('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length);
+            $(elt).keydown(function(){
+                    //alert("hi");                   
+                    if ($(elt).val().length > 160) {
+                    	//alert("hi no more");
+                    	$(elt).val($(elt).val().substring(0, 160));                    	
+                    }
+                     $('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length);                    
             }); 
         });
+    
 }     
