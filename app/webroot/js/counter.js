2cee91dce7259264d2eb08fce58b2fccb36e08a1
markphilip <markphi119@gmail.com>
2013-04-26 14:44:03 +0300

    character and message counter dispaly format changed

--- app/webroot/js/counter.js
+++ app/webroot/js/counter.js
@@ -19,7 +19,7 @@ function addCounter(){
             
             var $remaining = $('#remaining'),
             $messages = $remaining.next();     
-            $("<span style='float: right; padding-right: 13px;'> <span id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span> : <span id='messages'>0 SMS Message(s)</span></span>").insertBefore($(elt));            
+            $("<span style='float: right; padding-right: 13px;'> <span id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span> / <span id='messages'>0 </span></span>").insertBefore($(elt));            
             $(elt).keydown(function(){
                     //alert("hi");                   
                     var chars = this.value.length,
@@ -27,7 +27,7 @@ function addCounter(){
                     remaining = messages * 0 + (chars % (messages * 160) || messages * 160);                    
                     
                     $('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length); 
-                    $('[id="messages"]').text(messages + ' SMS Message(s)');	                     
+                    $('[id="messages"]').text(messages + '');	                     
             }); 
         });
     
