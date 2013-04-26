08f881dfe91413eb0201236d25da77a3afb34441
markphilip <markphi119@gmail.com>
2013-04-26 09:42:11 +0300

    char and message display changed

--- app/webroot/js/counter.js
+++ app/webroot/js/counter.js
@@ -14,11 +14,12 @@ function addCounter(){
             if ($(elt).val().length > 160) {
             //alert("hi no more");
             $(elt).val($(elt).val().substring(0, 160));                    	
-            } */   
+            } Characters
+            */   
             
             var $remaining = $('#remaining'),
             $messages = $remaining.next();     
-            $("<span style='float: right; padding-right: 13px;'> <span id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span> Characters <span id='messages'>0 message(s)</span></span>").insertBefore($(elt));            
+            $("<span style='float: right; padding-right: 13px;'> <span id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span> : <span id='messages'>0 SMS Message(s)</span></span>").insertBefore($(elt));            
             $(elt).keydown(function(){
                     //alert("hi");                   
                     var chars = this.value.length,
@@ -26,7 +27,7 @@ function addCounter(){
                     remaining = messages * 0 + (chars % (messages * 160) || messages * 160);                    
                     
                     $('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length); 
-                    $('[id="messages"]').text(messages + ' messages(s)');	                     
+                    $('[id="messages"]').text(messages + ' SMS Message(s)');	                     
             }); 
         });
     
