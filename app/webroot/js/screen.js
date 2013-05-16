47fcd9eb77a2b21487b2f3b155c50b13b1e073ed
markphilip <markphi119@gmail.com>
2013-05-16 17:28:20 +0300

    nice projection, java script chanage css on resize

--- app/webroot/js/screen.js
+++ app/webroot/js/screen.js
@@ -1,11 +1,17 @@
 
 function layoutHandler(){	
-	var styleLink = $(".width-size");	
+		alert("hi");
 	if (window.innerWidth > 800){
 		/*styleLink.setAttribute("href", "mobile.css")*/
 		$(".width-size").attr('width','1053;');		
 	}		
 }
 
-window.onresize = layoutHandler();
+$(function(){
+		layoutHandler($(this));
+		$(window).resize(function(){
+				layoutHandler($(this));
+		});
+});
+/*window.onresize = layoutHandler();*/
 
