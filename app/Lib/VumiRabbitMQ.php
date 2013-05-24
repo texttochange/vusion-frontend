e3b472f75feb89137f8c80b41756ff24e6531f68
Olivier Vernin <overnin@gmail.com>
2013-05-24 19:15:47 +0300

    make backend toleratnt to bad control message

--- app/Lib/VumiRabbitMQ.php
+++ app/Lib/VumiRabbitMQ.php
@@ -1,4 +1,6 @@
 <?php
+App::uses('WorkerMessageMaker', 'Lib');
+
 
 class VumiRabbitMQ {
 
@@ -22,7 +24,10 @@ class VumiRabbitMQ {
         $this->ch = $this->conn->channel();
         $this->ch->access_request('/data', false, false, true, true);
         $this->ch->exchange_declare($this->EXCHANGE, 'direct', false, true, false);
+
+        $this->workerMessageMaker = new WorkerMessageMaker();
     }
+
     
     function __destruct()
     {
@@ -30,105 +35,54 @@ class VumiRabbitMQ {
         $this->conn->close();
     }
 
+
     public function sendMessageToCreateWorker($application_name, $database_name, $dispatcher_name="dispatcher", $send_loop_period="60")
     {
-        return $this->sendMessageTo(
-            'vusion.control', 
-            array(
-                'message_type' => 'add_worker',
-                'worker_name' => $database_name,
-                'worker_class' => 'vusion.DialogueWorker',
-                'config' => array(
-                    'application_name' =>  $application_name,
-                    'transport_name' => $application_name,
-                    'control_name' => $application_name,
-                    'database_name' => $database_name,
-                    'vusion_database_name' => 'vusion',
-                    'dispatcher_name' => $dispatcher_name,
-                    'send_loop_period'=> $send_loop_period,
-                    )
-                ) 
-            );
+        $msg = $this->workerMessageMaker->createWorker($application_name, $database_name, $dispatcher_name, $send_loop_period);
+        return $this->sendMessageTo('vusion.control', $msg);
     }
+
 
     public function sendMessageToRemoveWorker($application_name, $database_name)
     {
-        return $this->sendMessageTo(
-            'vusion.control', 
-            array(
-                'message_type' => 'remove_worker',
-                'worker_name' => $database_name,
-                )
-            );
+        $msg = $this->workerMessageMaker->removeWorker($application_name, $database_name);
+        return $this->sendMessageTo('vusion.control', $msg);
     }
+
 
     public function sendMessageToUpdateSchedule($to, $schedule_type, $object_id)
     {
-        $message = array(
-            'action' => 'update_schedule',
-            'schedule_type' => $schedule_type,
-            'object_id' => $object_id );
-        
-        return $this->sendMessageTo(
-            $to.'.control',
-            $message
-            );
+        $msg = $this->workerMessageMaker->updateSchedule($schedule_type, $object_id);
+        return $this->sendMessageTo($to.'.control', $msg);
     }
+
 
     public function sendMessageToUpdateRegisteredKeywords($to)
     {
-        $message = array(
-            'action' => 'update_registered_keywords');
-        
-        return $this->sendMessageTo(
-            $to.'.control',
-            $message
-            );
+        $msg = $this->workerMessageMaker->updateRegisteredKeywords();
+        return $this->sendMessageTo($to.'.control', $msg);
     }
+
 
     public function sendMessageToReloadProgramSettings($to)
     {
-        $message = array(
-            'action' => 'reload_program_settings');
-        
-        return $this->sendMessageTo(
-            $to.'.control',
-            $message
-            );
-    }
-
-    public function sendMessageToSendAllMessages($to, $phone, $dialogueObjId)
-    {
-        return $this->sendMessageTo(
-            $to.'.control',
-            array(
-                'action' => 'test_send_all_messages',
-                'phone_number' => $phone,
-                'dialogue_obj_id' => $dialogueObjId)
-            );
+        $msg = $this->workerMessageMaker->reloadProgramSettings();
+        return $this->sendMessageTo($to.'.control', $msg);
     }
 
 
-    public function sendMessageToWorker($to, $from, $msg)
+    public function sendMessageToSendAllMessages($to, $phone, $dialogue_object_id)
     {
-         return $this->sendMessageTo(
-             $to.'.inbound',
-             array(
-                 "content" => $msg, 
-                 "message_version" => '20110921',
-                 "message_type" => '', 
-                 "timestamp" =>"", 
-                 "message_id" => "", 
-                 "to_addr" => "0.0.0.0:9020",
-                 "from_addr" => $from,
-                 "in_reply_to" => "",
-                 "session_event"=> null,
-                 "transport_name" =>"",
-                 "transport_type" => "",
-                 "transport_metadata" => "",
-                 "helper_metadata" => "")
-             );
-    }    
+        $msg = $this->workerMessageMaker->testSendAllMessages($phone, $dialogue_object_id);
+        return $this->sendMessageTo($to.'.control', $msg);
+    }
+
+
+    public function sendMessageToWorker($to, $from, $content)
+    {
+        $msg = $this->workerMessageMaker->transportUserMessage($from, $content);
+        return $this->sendMessageTo($to.'.inbound', $msg);
+    }
 
 
     public function sendMessageTo($to, $msg)
