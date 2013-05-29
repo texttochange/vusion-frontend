50e082148ea66d13f02fb4348fc09fec17ddb016
Olivier Vernin <overnin@gmail.com>
2013-05-29 10:08:58 +0300

    fix syntax

--- app/Lib/VusionConst.php
+++ app/Lib/VusionConst.php
@@ -3,7 +3,7 @@
 class VusionConst
 {
     const KEYWORD_REGEX = '/^[a-zA-Z0-9]+(,(\s)?[a-zA-Z0-9]+)*$/';
-    const KEYWORD_FAIL_MESSAGE = 'The keyword/alias is(are) not valid.'
+    const KEYWORD_FAIL_MESSAGE = 'The keyword/alias is(are) not valid.';
 
     const TAG_REGEX='/^[a-z0-9A-Z\s]+$/';
     const TAG_FAIL_MESSAGE='Only letters and numbers. Must be tag, tag, ... e.g cool, nice, ...';
