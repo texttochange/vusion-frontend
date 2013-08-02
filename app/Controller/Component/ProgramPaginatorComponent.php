dbfed48b94b945ae1d4bf45ced15eefb6ebde9f7
markphilip <markphi119@gmail.com>
2013-08-02 12:14:28 +0300

    checkstyle fix for statsComponent and programController

--- app/Controller/Component/ProgramPaginatorComponent.php
+++ app/Controller/Component/ProgramPaginatorComponent.php
@@ -2,23 +2,23 @@
 App::uses('Component', 'Controller');
 App::uses('ShortCode', 'Model');
 
-class ProgramPaginatorComponent extends Component {
+class ProgramPaginatorComponent extends Component 
+{  
+    
+    public $components = array('Stats'); 
+    
+    var $settings = array(
+        'page' => 1,
+        'limit' => 20,
+        'maxLimit' => 100,
+        'paramType' => 'named'
+    );
     
     
-	public $components = array('Stats'); 
-	
-	var $settings = array(
-		'page' => 1,
-		'limit' => 20,
-		'maxLimit' => 100,
-		'paramType' => 'named'
-	);
-	
-	
-	public function __construct($collection, $settings = array())
-	{
-	    $settings = array_merge($this->settings, (array)$settings);
-	    $this->Controller = $collection->getController();
+    public function __construct($collection, $settings = array())
+    {
+        $settings = array_merge($this->settings, (array)$settings);
+        $this->Controller = $collection->getController();
         parent::__construct($collection, $settings);
 
         if (!Configure::read("mongo_db")) {
@@ -31,53 +31,53 @@ class ProgramPaginatorComponent extends Component {
                 );
         }
         $this->ShortCode  = new ShortCode($options);
-	}
+    }
     
     public function paginate($paginateArray)
     {
         if (!is_array($paginateArray)) {
-			throw new MissingModelException($paginateArray);
-		}
-		
-		$object = $this->Controller->uses[0];
-		$options = array();
-		$params = $this->Controller->request->params;		
-		$limit = (int)$this->settings['limit'];
-		$page = $options['page'] = (isset($params['named']['page'])) ? (int)$params['named']['page'] : 1;		
-		$order = null;
-		$count =  count($paginateArray);
-		$pageCount = intVal(ceil($count / $limit));
-		$page_offset = ($page - 1) * $limit;
-		
-		$results = array_slice($paginateArray, $page_offset, $limit);
+            throw new MissingModelException($paginateArray);
+        }
+        
+        $object = $this->Controller->uses[0];
+        $options = array();
+        $params = $this->Controller->request->params;
+        $limit = (int)$this->settings['limit'];
+        $page = $options['page'] = (isset($params['named']['page'])) ? (int)$params['named']['page'] : 1;
+        $order = null;
+        $count =  count($paginateArray);
+        $pageCount = intVal(ceil($count / $limit));
+        $page_offset = ($page - 1) * $limit;
+        
+        $results = array_slice($paginateArray, $page_offset, $limit);
 
-		$paging = array(
-			'page' => $page,
-			'current' => count($results),
-			'count' => $count,
-			'prevPage' => ($page > 1),
-			'nextPage' => ($count > ($page * $limit)),
-			'pageCount' => $pageCount,
-			'order' => $order,
-			'limit' => $limit,
-			'options' => $options,
-			'paramType' => $this->settings['paramType']
-		);
+        $paging = array(
+            'page' => $page,
+            'current' => count($results),
+            'count' => $count,
+            'prevPage' => ($page > 1),
+            'nextPage' => ($count > ($page * $limit)),
+            'pageCount' => $pageCount,
+            'order' => $order,
+            'limit' => $limit,
+            'options' => $options,
+            'paramType' => $this->settings['paramType']
+        );
 
-		# Use pagintor helper in the view to display our results
-		if (!isset($this->Controller->request['paging'])) {
-			$this->Controller->request['paging'] = array();
-		}
-		$this->Controller->request['paging'] = array_merge(
-			(array)$this->Controller->request['paging'],
-			array($object => $paging)
-		);
+        # Use pagintor helper in the view to display our results
+        if (!isset($this->Controller->request['paging'])) {
+            $this->Controller->request['paging'] = array();
+        }
+        $this->Controller->request['paging'] = array_merge(
+            (array)$this->Controller->request['paging'],
+            array($object => $paging)
+        );
 
-		if (!in_array('Paginator', $this->Controller->helpers) &&
-			!array_key_exists('Paginator', $this->Controller->helpers))
-		{
-			$this->Controller->helpers[] = 'Paginator';
-		}
+        if (!in_array('Paginator', $this->Controller->helpers) &&
+            !array_key_exists('Paginator', $this->Controller->helpers))
+        {
+            $this->Controller->helpers[] = 'Paginator';
+        }
         return $results;
     }
 
--- app/Test/Case/Controller/Component/StatsComponentTest.php
+++ app/Test/Case/Controller/Component/StatsComponentTest.php
@@ -12,109 +12,111 @@ App::uses('ProgramSetting', 'Model');
 
 
 
-class TestStatsComponentController extends Controller {
+class TestStatsComponentController extends Controller 
+{  
 	
-	var $components = array('Stats');
-	
-	function constructClasses()
-	{
-		$this->redis = new Redis();
-		$this->redis->connect('127.0.0.1');
-		$this->redisProgramPrefix = 'unittest';
-	}
+    var $components = array('Stats');
+    
+    function constructClasses()
+    {
+        $this->redis = new Redis();
+        $this->redis->connect('127.0.0.1');
+        $this->redisProgramPrefix = 'unittest';
+    }
 }
 
 
-class StatsComponentTest extends CakeTestCase {
+class StatsComponentTest extends CakeTestCase 
+{
 	
-	public $StatsComponent = null;
-	public $Controller = null;
-	
-	public function setUp()
-	{
-		parent::setUp();
-		$Collection = new ComponentCollection();
-		$this->StatsComponent = new StatsComponent($Collection);
-		$CakeRequest = new CakeRequest();
-		$CakeResponse = new CakeResponse();
-		
-		$this->Controller = new TestStatsComponentController($CakeRequest, $CakeResponse);
-		$this->Controller->constructClasses();
-		$this->StatsComponent->initialize($this->Controller);
-		$this->redis = $this->Controller->redis;
-		
-		$this->Maker = new ScriptMaker();
-		$options = array('database' => 'testdbprogram');
-		$this->instanciateModels($options);
-		$this->ProgramSetting->saveProgramSetting('timezone','Africa/Kampala');
-	}
-	
-	protected function dropData()
-	{
-		$this->Participant->deleteAll(true, false);
-		$this->Schedule->deleteAll(true, false);
-		$this->History->deleteAll(true, false);
-		$this->ProgramSetting->deleteAll(true, false);
-	}
-	
-	protected function instanciateModels($options)
+    public $StatsComponent = null;
+    public $Controller = null;
+    
+    public function setUp()
     {
-		$this->Participant = new Participant($options);
-		$this->Schedule = new Schedule($options);
-		$this->History = new History($options);
-		$this->ProgramSetting = new ProgramSetting($options); 
+        parent::setUp();
+        $Collection = new ComponentCollection();
+        $this->StatsComponent = new StatsComponent($Collection);
+        $CakeRequest = new CakeRequest();
+        $CakeResponse = new CakeResponse();
+        
+        $this->Controller = new TestStatsComponentController($CakeRequest, $CakeResponse);
+        $this->Controller->constructClasses();
+        $this->StatsComponent->initialize($this->Controller);
+        $this->redis = $this->Controller->redis;
+        
+        $this->Maker = new ScriptMaker();
+        $options = array('database' => 'testdbprogram');
+        $this->instanciateModels($options);
+        $this->ProgramSetting->saveProgramSetting('timezone','Africa/Kampala');
     }
-	
-	public function tearDown()
-	{
-		$keys = $this->redis->keys('unittest*');
-		foreach ($keys as $key){
-			$this->redis->delete($key);
-		}
-		$this->dropData();
-		unset($this->StatsComponent);
-		parent::tearDown();
-	}
-	
-	public function mkStats()
-	{
-		return array(
-			'active-participant-count' => '2',
-			'participant-count' => '2',
-			'all-received-messages-count'=> '2',
-			'current-month-received-messages-count' => '2',
-			'all-sent-messages-count' => '2',
-			'current-month-sent-messages-count' => '2',
-			'total-current-month-messages-count' => '2',
-			'history-count' => '1',
-			'today-schedule-count' => '2',
-			'schedule-count' => '2',
-			'object-type' => 'program-stats',
-			'model-version'=> '1'
-			);
-	}
-	
-	public function testGetStats()
-	{
-		$this->redisProgramPrefix = 'unittest';		
-		$testStats = $this->mkStats();
-		$program = array(
-			'Program'=> array(
-				'database' => 'testdbprogram'));
-		$key = $this->redisProgramPrefix.':'.$program['Program']['database'].':stats';
-		
-		$this->redis->set($key, json_encode($testStats));
-		$programTestStats = $this->StatsComponent->getProgramStats($program['Program']['database']);
-		
-		$this->assertEqual(
-			$testStats,
-			$programTestStats);
-	}
-	
-	public function testGetStats_noStatsInRedis()
-	{
-		$this->Participant->create();
-		$savedParticipant = $this->Participant->save($this->Maker->getParticipant());
+    
+    protected function dropData()
+    {
+        $this->Participant->deleteAll(true, false);
+        $this->Schedule->deleteAll(true, false);
+        $this->History->deleteAll(true, false);
+        $this->ProgramSetting->deleteAll(true, false);
+    }
+    
+    protected function instanciateModels($options)
+    {
+        $this->Participant = new Participant($options);
+        $this->Schedule = new Schedule($options);
+        $this->History = new History($options);
+        $this->ProgramSetting = new ProgramSetting($options); 
+    }
+    
+    public function tearDown()
+    {
+        $keys = $this->redis->keys('unittest*');
+        foreach ($keys as $key){
+            $this->redis->delete($key);
+        }
+        $this->dropData();
+        unset($this->StatsComponent);
+        parent::tearDown();
+    }
+    
+    public function mkStats()
+    {
+        return array(
+            'active-participant-count' => '2',
+            'participant-count' => '2',
+            'all-received-messages-count'=> '2',
+            'current-month-received-messages-count' => '2',
+            'all-sent-messages-count' => '2',
+            'current-month-sent-messages-count' => '2',
+            'total-current-month-messages-count' => '2',
+            'history-count' => '1',
+            'today-schedule-count' => '2',
+            'schedule-count' => '2',
+            'object-type' => 'program-stats',
+            'model-version'=> '1'
+            );
+    }
+    
+    public function testGetStats()
+    {
+        $this->redisProgramPrefix = 'unittest';        
+        $testStats = $this->mkStats();
+        $program = array(
+            'Program'=> array(
+                'database' => 'testdbprogram'));
+        $key = $this->redisProgramPrefix.':'.$program['Program']['database'].':stats';
+        
+        $this->redis->set($key, json_encode($testStats));
+        $programTestStats = $this->StatsComponent->getProgramStats($program['Program']['database']);
+        
+        $this->assertEqual(
+            $testStats,
+            $programTestStats);
+    }
+    
+    public function testGetStats_noStatsInRedis()
+    {
+        $this->Participant->create();
+        $savedParticipant = $this->Participant->save($this->Maker->getParticipant());
         $this->Schedule->create('dialogue-schedule');
         $saveSchedule = $this->Schedule->save(array(
             'Schedule' => array(
@@ -122,7 +124,7 @@ class StatsComponentTest extends CakeTestCase {
                 'dialogue-id' => 'def456',
                 )
             )); 
-		$this->History->create('unattach-history');
+        $this->History->create('unattach-history');
         $savedHistory = $this->History->save(array(
             'participant-phone' => '256712747841',
             'message-content' => 'Hello everyone!',
@@ -139,18 +141,18 @@ class StatsComponentTest extends CakeTestCase {
             'message-status' => 'delivered',
             ));
         
-		$key = "unittest:testdbprogram:stats";
-		
-		$stats = $this->redis->get($key);
-		$programTestStats = $this->StatsComponent->getProgramStats('testdbprogram');
-		
-		$this->assertEqual(
-			null,
-			$stats);
-		
-		$this->assertEqual(
-			'2',
-			$programTestStats['history-count']);
-	}
+        $key = "unittest:testdbprogram:stats";
+        
+        $stats = $this->redis->get($key);
+        $programTestStats = $this->StatsComponent->getProgramStats('testdbprogram');
+        
+        $this->assertEqual(
+            null,
+            $stats);
+        
+        $this->assertEqual(
+            '2',
+            $programTestStats['history-count']);
+    }
 }
 ?>
