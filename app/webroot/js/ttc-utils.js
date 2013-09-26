a6a37aa6f55a53a144ef0d319dd04116a3bb6fcd
markphilip <markphi119@gmail.com>
2013-09-26 12:59:38 +0300

    removed substring and used regexp

--- app/webroot/js/ttc-utils.js
+++ app/webroot/js/ttc-utils.js
@@ -538,7 +538,7 @@ function loadProgramStats(){
 					},
 					timeout: 360000,  // 6 minutes
 					error: function(){
-						$("#"+this.url.substring(42)+" .ttc-program-stats").empty().append(generateHtmlProgramStats())
+						$("#"+this.url.match(/programUrl=(.*)+/)[1]+" .ttc-program-stats").empty().append(generateHtmlProgramStats())
 					}
 			});
 		}
