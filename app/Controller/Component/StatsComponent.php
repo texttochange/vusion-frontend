55a33394224d0c112f5c56853e0bc3c03ad939ef
markphilip <markphi119@gmail.com>
2013-10-03 16:46:51 +0300

    removed print_r

--- app/Controller/Component/StatsComponent.php
+++ app/Controller/Component/StatsComponent.php
@@ -53,98 +53,95 @@ class StatsComponent extends Component
             'object-type' => 'program-stats',
             'model-version'=> '1');
         
-            $this->ProgramSetting = new ProgramSetting(array('database' => $database));
-            $programTimeNow = $this->ProgramSetting->getProgramTimeNow();            
-            if(empty($programTimeNow)){
-                return $programStats;
-            }
-            $tempParticipant = new Participant(array('database' => $database));                
-            $activeParticipantCount = $tempParticipant->find(
-                'count', array(
-                    'conditions' => array('session-id' => array(
-                        '$ne' => null)
-                        )
+        $this->ProgramSetting = new ProgramSetting(array('database' => $database));
+        $programTimeNow = $this->ProgramSetting->getProgramTimeNow();            
+        if(empty($programTimeNow)){
+            return $programStats;
+        }
+        $tempParticipant = new Participant(array('database' => $database));                
+        $activeParticipantCount = $tempParticipant->find(
+            'count', array(
+                'conditions' => array('session-id' => array(
+                    '$ne' => null)
                     )
-                );
-            $programStats['active-participant-count'] = $this->_getProgramStat($tempParticipant, $activeParticipantCount);
-           
-            $participantCount = $tempParticipant->find('count'); 
-            $programStats['participant-count'] = $this->_getProgramStat($tempParticipant, $participantCount);
-            
-            $tempSchedule = new Schedule(array('database' => $database));
-            $programTimeToday = $programTimeNow->modify('+1 day');
-            $todayScheduleCount = $tempSchedule->find(
-                'count',array(
-                    'conditions' => array(
-                        'date-time' => array(
-                            '$lt' => $programTimeToday->format(DateTime::ISO8601))
-                        )
-                    ));
-            $programStats['today-schedule-count'] = $this->_getProgramStat($tempSchedule, $todayScheduleCount);
-            
-            $scheduleCount = $tempSchedule->find('count');
-            $programStats['schedule-count'] = $this->_getProgramStat($tempSchedule, $scheduleCount);
-            
-            $tempHistory     = new History(array('database' => $database));
-            $programTimeForMonth = $programTimeNow->format("Y-m-d\TH:i:s");        
-            $first_second = date('Y-m-01\TH:i:s', strtotime($programTimeForMonth));
-            $last_second = date('Y-m-t\TH:i:s', strtotime($programTimeForMonth));
-            
-            $allReceivedMessagesCount = $tempHistory->find(
-                'count',array(
-                    'conditions' => array('message-direction' => 'incoming'))
-                );
-            $programStats['all-received-messages-count'] = $this->_getProgramStat($tempHistory, $allReceivedMessagesCount);
-            
-            $currentMonthReceivedMessagesCount = $tempHistory->find(
-                'count',array(
-                    'conditions' => array(
-                        'timestamp' => array(
-                            '$gt' => $first_second,
-                            '$lt' => $last_second
-                            ),
-                        'message-direction' => 'incoming'
-                        )
+                )
+            );
+        $programStats['active-participant-count'] = $this->_getProgramStat($tempParticipant, $activeParticipantCount);
+        
+        $participantCount = $tempParticipant->find('count'); 
+        $programStats['participant-count'] = $this->_getProgramStat($tempParticipant, $participantCount);
+        
+        $tempSchedule = new Schedule(array('database' => $database));
+        $programTimeToday = $programTimeNow->modify('+1 day');
+        $todayScheduleCount = $tempSchedule->find(
+            'count',array(
+                'conditions' => array(
+                    'date-time' => array(
+                        '$lt' => $programTimeToday->format(DateTime::ISO8601))
                     )
-                );
-            $programStats['current-month-received-messages-count'] = $this->_getProgramStat($tempHistory, $currentMonthReceivedMessagesCount);
-            
-            $currentMonthSentMessagesCount = $tempHistory->find(
-                'count',array(
-                    'conditions' => array(
-                        'timestamp' => array(
-                            '$gt' => $first_second,
-                            '$lt' => $last_second
-                            ),
-                        'message-direction' => 'outgoing'
-                        )
+                ));
+        $programStats['today-schedule-count'] = $this->_getProgramStat($tempSchedule, $todayScheduleCount);
+        
+        $scheduleCount = $tempSchedule->find('count');
+        $programStats['schedule-count'] = $this->_getProgramStat($tempSchedule, $scheduleCount);
+        
+        $tempHistory     = new History(array('database' => $database));
+        $programTimeForMonth = $programTimeNow->format("Y-m-d\TH:i:s");        
+        $first_second = date('Y-m-01\TH:i:s', strtotime($programTimeForMonth));
+        $last_second = date('Y-m-t\TH:i:s', strtotime($programTimeForMonth));
+        
+        $allReceivedMessagesCount = $tempHistory->find(
+            'count',array(
+                'conditions' => array('message-direction' => 'incoming'))
+            );
+        $programStats['all-received-messages-count'] = $this->_getProgramStat($tempHistory, $allReceivedMessagesCount);
+        
+        $currentMonthReceivedMessagesCount = $tempHistory->find(
+            'count',array(
+                'conditions' => array(
+                    'timestamp' => array(
+                        '$gt' => $first_second,
+                        '$lt' => $last_second
+                        ),
+                    'message-direction' => 'incoming'
                     )
-                );
-            $programStats['current-month-sent-messages-count'] = $this->_getProgramStat($tempHistory, $currentMonthSentMessagesCount);
-            
-            $totalCurrentMonthMessagesCount = $currentMonthSentMessagesCount + $currentMonthReceivedMessagesCount;
-            $programStats['total-current-month-messages-count'] = $totalCurrentMonthMessagesCount;
-            
-            $allSentMessagesCount = $tempHistory->find(
-                'count',array(
-                    'conditions' => array('message-direction' => 'outgoing'))
-                );
-            $programStats['all-sent-messages-count'] = $this->_getProgramStat($tempHistory, $allSentMessagesCount);
-            
-            $historyCount  = $tempHistory->find(
-                'count', array(
-                    'conditions' =>array(
-                        '$or' => array(
-                            array('object-type' => array('$in' => $tempHistory->messageType)),
-                            array('object-type' => array('$exists' => false ))
-                            )
-                        )));
-            $programStats['history-count'] = $this->_getProgramStat($tempHistory, $historyCount);  
-            
-            $programStats['object-type'] = 'program-stats';
-            $programStats['model-version'] = '1';
-            print_r($programStats);
-            return $programStats;        
+                )
+            );
+        $programStats['current-month-received-messages-count'] = $this->_getProgramStat($tempHistory, $currentMonthReceivedMessagesCount);
+        
+        $currentMonthSentMessagesCount = $tempHistory->find(
+            'count',array(
+                'conditions' => array(
+                    'timestamp' => array(
+                        '$gt' => $first_second,
+                        '$lt' => $last_second
+                        ),
+                    'message-direction' => 'outgoing'
+                    )
+                )
+            );
+        $programStats['current-month-sent-messages-count'] = $this->_getProgramStat($tempHistory, $currentMonthSentMessagesCount);
+        
+        $totalCurrentMonthMessagesCount = $currentMonthSentMessagesCount + $currentMonthReceivedMessagesCount;
+        $programStats['total-current-month-messages-count'] = $totalCurrentMonthMessagesCount;
+        
+        $allSentMessagesCount = $tempHistory->find(
+            'count',array(
+                'conditions' => array('message-direction' => 'outgoing'))
+            );
+        $programStats['all-sent-messages-count'] = $this->_getProgramStat($tempHistory, $allSentMessagesCount);
+        
+        $historyCount  = $tempHistory->find(
+            'count', array(
+                'conditions' =>array(
+                    '$or' => array(
+                        array('object-type' => array('$in' => $tempHistory->messageType)),
+                        array('object-type' => array('$exists' => false ))
+                        )
+                    )));
+        $programStats['history-count'] = $this->_getProgramStat($tempHistory, $historyCount);  
+        
+        return $programStats;        
     }
     
     protected function _getStatsKey($database)
