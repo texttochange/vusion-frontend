a6ab6bfbc0a142b181aa38b8729672cb202cbc82
markphilip <markphi119@gmail.com>
2014-01-21 16:39:27 +0300

    added mouseenter and change events

--- app/webroot/js/counter.js
+++ app/webroot/js/counter.js
@@ -6,10 +6,13 @@ function addCounter(){
                 return;
             }
             $("<span style='float: right; padding-right: 13px;'> <span id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span> Characters</span>").insertBefore($(elt));
-            $(elt).focus(function(){
+            $('#predefined-message').on('change', function(event){
+            	$('[id="'+$(elt).attr('name')+'"]').text($(elt).val().replace(/{.*}/g, '').length);            	
+            });
+            $(elt).mouseenter(function(){
             	$('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length);            	
             }).keyup(function(){                   
-                    $('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length);
+                $('[id="'+$(elt).attr('name')+'"]').text($(this).val().replace(/{.*}/g, '').length);
             });
         });
 }  
