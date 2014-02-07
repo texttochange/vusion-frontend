a53bc0716442b8906d9aa70dc32996e20480b7b0
Olivier Vernin <overnin@gmail.com>
2014-02-07 10:23:32 +0300

    fix clean up deprecated code

--- app/Test/Case/Helper/DialogueHelperTest.php
+++ app/Test/Case/Helper/DialogueHelperTest.php
@@ -13,104 +13,6 @@ class DialogueHelperTestCase extends CakeTestCase
         $this->Maker = new ScriptMaker();
     }
   
-/*   
-    #Deprecated
-    public function testHasKeyword()
-    {
-        $dialogue = $this->Maker->getOneDialogue('kÉyword');
-        $this->assertEquals(
-            array('keyword'),
-            DialogueHelper::hasKeywords($dialogue, array('keyword')));
-        
-        $dialogue = $this->Maker->getOneDialogueWithKeyword();
-        $this->assertEquals(
-            array('feel', 'keyword'),
-            DialogueHelper::hasKeywords($dialogue, array('feel', 'keyword')));
-        
-        $dialogue = $this->Maker->getOneDialogueWithKeyword('keyword');
-        $this->assertEquals(
-            array('keyword'),
-            DialogueHelper::hasKeywords($dialogue, array('keyword')));
-    }
-
-    #Deprecated
-    public function testHasKeywork_answerAcceptNoSpace()
-    {
-        $dialogue = $this->Maker->getOneDialogueAnwerNoSpaceSupported('fÉl');
-        $this->assertEquals(
-            array('felgood'),
-            DialogueHelper::hasKeywords($dialogue, array('felgood')));
-        $this->assertEquals(
-            array('fel'),
-            DialogueHelper::hasKeywords($dialogue, array('fel')));
-        $this->assertEquals(
-            array('felbad'),
-            DialogueHelper::hasKeywords($dialogue, array('felbad')));
-        $this->assertEquals(
-            array(),
-            DialogueHelper::hasKeywords($dialogue, array('felok')));
-    }
-
-    #Deprecated
-    public function testGetKeyworks()
-    {
-        $dialogue = $this->Maker->getOneDialogueAnwerNoSpaceSupported('fÉl');
-        $this->assertEquals(
-            array('feel', 'fel', 'felgood','felbad'), 
-            DialogueHelper::getKeywords($dialogue));
-
-        $dialogue = $this->Maker->getOneDialogue('usedKeyword');
-        $this->assertEquals(
-            array('usedkeyword'), 
-            DialogueHelper::getKeywords($dialogue));
-        
-        $dialogue = $this->Maker->getOneDialogueMultikeyword();
-        $this->assertEqual(
-            array('female', 'male'),
-            DialogueHelper::getKeywords($dialogue));
-    }
-
-    #Deprecated
-    public function testHasNoMatchingAnswers()
-    {
-        $dialogue = array('keyword'=>'keyword',
-        	'answers'=> array(
-        	    0 => array('choice' => 'Bad'),
-        	    1 => array('choice' => 'Good')
-        	)
-        );
-        
-        $status = array('message-content'=>'keyword Good');
-        $this->assertFalse(DialogueHelper::hasNoMatchingAnswers($dialogue, $status));
-                
-        $status = array('message-content'=>'keyword Goo');
-        $this->assertTrue(DialogueHelper::hasNoMatchingAnswers($dialogue, $status));
-        
-        $dialogue = $this->Maker->getOneDialogueWithKeyword('keyword');
-        $status = array('message-content'=>'keyword Bad');
-        $this->assertFalse(DialogueHelper::hasNoMatchingAnswers($dialogue, $status));
-        
-        $status = array('message-content'=>'keyword 2');
-        $this->assertFalse(DialogueHelper::hasNoMatchingAnswers($dialogue, $status));
-    }
-
-    #Deprecated
-    public function testGetInteraction()
-    {
-        $dialogue = $this->Maker->getOneDialogueWithKeyword('keyword');
-        $this->assertEqual(
-            $dialogue['Dialogue']['interactions'][0],	
-            DialogueHelper::getInteraction($dialogue, "script.dialogues[0].interactions[0]"));
-        
-        $this->assertEqual(
-            $dialogue['Dialogue']['interactions'][1],	
-            DialogueHelper::getInteraction($dialogue, "script.dialogues[0].interactions[1]"));
-        
-        $this->assertEqual(
-            array(),	
-            DialogueHelper::getInteraction($dialogue, "other"));
-    }
-*/
 
     public function testGetRequestKeywordToValidate()
     {
@@ -133,33 +35,6 @@ class DialogueHelperTestCase extends CakeTestCase
         $this->assertEqual(null, DialogueHelper::convertDateFormat(''));
         $this->assertEqual(null, DialogueHelper::convertDateFormat(null));
     }
-
-
-    //TODO: Fail to work well with unicode character with accent
- /* 
-    #Deprecated  
-    public function testIsUsedKeyword()
-    {
-        $usedKeywords = array(
-            'keyword1' => array('programName' => 'myprog1', 'type' => 'dialogue'),
-            'keyword2' => array('programName' => 'myprog2', 'type' => 'dialogue'),
-            'kéÉywórd2' => array('programName' => 'myprog2', 'type' => 'dialogue'));
-
-        $this->assertEqual(
-            array('keyword1' => array('programName' => 'myprog1', 'type' => 'dialogue')),
-            DialogueHelper::isUsedKeyword('keyword1', $usedKeywords));
-
-        $this->assertEqual(
-            array('keyword2' => array('programName' => 'myprog2', 'type' => 'dialogue')),
-            DialogueHelper::isUsedKeyword('keyWord2', $usedKeywords));
-        $this->assertEqual(
-            array('keyword2' => array('programName' => 'myprog2', 'type' => 'dialogue')),
-            DialogueHelper::isUsedKeyword('keYword2', $usedKeywords));
-
-        $this->assertEqual(
-            array('kéÉywórd2' => array('programName' => 'myprog2', 'type' => 'dialogue')),
-            DialogueHelper::isUsedKeyword('kéÉywórd2', $usedKeywords));
-    }*/
 
 
     public function testCleanKeyword()
@@ -207,6 +82,27 @@ class DialogueHelperTestCase extends CakeTestCase
         $this->assertEqual(
             DialogueHelper::cleanKeywords($keywords), 
             array('k1', 'k2', 'k3', 'k4', 'k5'));
+    }
+
+
+    public function testCleanKeyword_numeric()
+    {
+        $this->assertEqual(
+            DialogueHelper::cleanKeyword('11'), 
+            '11');
+    }
+
+
+    public function testCleanPhrase_numeric()
+    {
+        $this->assertEqual(
+            DialogueHelper::cleanKeyphrases('11'), 
+            array('11'));
+
+        $this->assertEqual(
+            DialogueHelper::cleanKeyphrases('11 something'), 
+            array('11 something'));
+
     }
 
     
