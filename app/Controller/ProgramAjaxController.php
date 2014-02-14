e77251db4efe9ade0c3465ff9c396a90ce9939f2
markphilip <markphi119@gmail.com>
2014-02-14 17:29:42 +0300

    i have failed to get the cache

--- app/Controller/ProgramAjaxController.php
+++ app/Controller/ProgramAjaxController.php
@@ -7,22 +7,27 @@ class ProgramAjaxController extends AppController
     var $components = array(
         'RequestHandler',
         'Stats');
-    var $uses = array('Program', 'Group');
-
+    
     function constructClasses()
     {
         parent::constructClasses();
     }
     
-    public function beforeFilter()
-    {
-        parent::beforeFilter();
-        $this->RequestHandler->accepts('json');
-    }
-    
     
     public function getProgramStatsCached()
     { 
-        
+        print_r($this->RequestHandler);
+        //print_r($this->params['program']);
+       // $t = $this->response->not_modified($this->Stats->getProgramStats($programUrl[0]['Program']['database']));
+       //print_r($t);
+         /*$programUrl = $this->params['url']['programUrl'];
+            if(count($programUrl) > 0){           
+                $programStats = $this->Stats->getProgramStats($programUrl[0]['Program']['database'], true);
+                $result = array('status' =>'ok', 'programUrl' => $programUrl[0]['Program']['url'], 'programStats' =>  programStats);
+            }else{
+                $result = array('status' =>'fail', 'programUrl' => $programParamsUrl, 'reason' => "This program url ". $programParamsUrl." doesn't exist", 'programStats' => null);
+            }
+            $this->set(compact('result'));
+        } */
     }
 }
