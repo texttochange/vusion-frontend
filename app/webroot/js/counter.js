79d13ad673d678d0f5c3017d61f1f9f6a09beced
tamale <tamale@tamale-VirtualBox.(none)>
2014-03-04 12:56:37 +0300

    change class name to sms-message-counter

--- app/webroot/js/counter.js
+++ app/webroot/js/counter.js
@@ -5,7 +5,7 @@ function addCounter(){
             if ($(elt).prev('span').length>0) {
                 return;
             }
-            $("<span style='float: right; padding-right: 13px;'> <span class='counter' id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span>"+localize_label('characters')+"</span>").insertBefore($(elt));
+            $("<span style='float: right; padding-right: 13px;'> <span class='sms-message-counter' id="+$(elt).attr('name')+">"+$(elt).val().replace(/{.*}/g, '').length+"</span>"+localize_label('characters')+"</span>").insertBefore($(elt));
             $('#predefined-message').on('change', function(event){
             	$('[id="'+$(elt).attr('name')+'"]').text($(elt).val().replace(/{.*}/g, '').length);            	
             });
