c9c411a6dd4473a83e683d6d89be97d1cb6583b9
markphilip <markphi119@gmail.com>
2014-03-13 15:19:27 +0300

    refactored the function to apply DRY

--- app/webroot/js/ttc-utils.js
+++ app/webroot/js/ttc-utils.js
@@ -20,18 +20,22 @@ function getNewDateUsingTimezone(){
 function addContentFormHelp(baseUrl) {
     if (!baseUrl)
         baseUrl="../.."
-    $.each($("[name*='content']").prev(":not(:has(img)):not(div):not(span)"),
-            function (key, elt){
-                    $("<img class='ttc-help' src='/img/help-icon-16.png'/>").appendTo($(elt)).click(function(){requestHelp(this, baseUrl, 'content')});
-            });
-    $.each($("[name*='[template]']").prev(":not(:has(img)):not(div)"),
-            function (key, elt){ 
-                    $("<img class='ttc-help' src='/img/help-icon-16.png'/>").appendTo($(elt)).click(function(){requestHelp(this, baseUrl, 'template')});
-            });
-    $.each($("[name*='\.keyword']").prev("label").not(":has(img)"),
-            function (key, elt){
-                    $("<img class='ttc-help' src='/img/help-icon-16.png'/>").appendTo($(elt)).click(function(){requestHelp(this, baseUrl, 'keyword')});
-            });
+    addFormHelp(baseUrl, 'content');
+    addFormHelp(baseUrl, 'template');
+    addFormHelp(baseUrl, 'keyword');
+    addFormHelp(baseUrl, 'forward-url');
+}
+
+
+function addFormHelp(baseUrl, name) {
+    var arr = [$("[name*='content']").prev(":not(:has(img)):not(div):not(span)"),
+        $("[name*='[template]']").prev(":not(:has(img)):not(div)"), 
+        $("[name*='\.keyword']").prev("label").not(":has(img)"),
+    $("[name*='\.forward-url']").prev("label").not(":has(img)")];
+    $.each(arr,
+        function (key, elt){
+            $("<img class='ttc-help' src='/img/help-icon-16.png'/>").appendTo($(elt)).click(function(){requestHelp(this, baseUrl, name)});
+        });
 }
 
 
