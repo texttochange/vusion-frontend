b60ed4eff57de24c7cce155fe0e06bbe167ce1c9
markphilip <markphi119@gmail.com>
2014-06-23 16:56:50 +0300

    fixed indentation issues

--- app/Controller/Component/MessageComponent.php
+++ app/Controller/Component/MessageComponent.php
@@ -4,22 +4,22 @@ App::uses('Component', 'Controller');
 
 class MessageComponent extends Component 
 {
-
+    
     var $localizedValueLabels = array();
-
+    
     public function __construct(ComponentCollection $collection, $settings = array())
     {
-    	$this->localizedValueLabels = array(
- 			"name" => __('Name'),
-		    "characters" => __('characters'),
-		    "content" => __('Content'),
-		    "immediately" => __('Immediately'),
-	 	   	"fixed-time" => __('Fixed time'),
-		    "save"=> __('Save'),
-		    "any"=> __('any'),
-		    "all"=> __('all'),
-		  	);
-	    $this->Controller = $collection->getController();
+        $this->localizedValueLabels = array(
+            "name" => __('Name'),
+            "characters" => __('characters'),
+            "content" => __('Content'),
+            "immediately" => __('Immediately'),
+            "fixed-time" => __('Fixed time'),
+            "save"=> __('Save'),
+            "any"=> __('any'),
+            "all"=> __('all'),
+            );
+        $this->Controller = $collection->getController();
         parent::__construct($collection, $settings);
         $this->Controller->set('messageLabels', $this->localizedValueLabels);
     }
