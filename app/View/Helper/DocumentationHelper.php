f92750b3d412b5afb4fe5d1f3699a661af411273
markphilip <markphi119@gmail.com>
2014-07-24 18:44:28 +0300

    fixed check style

--- app/View/Helper/DocumentationHelper.php
+++ app/View/Helper/DocumentationHelper.php
@@ -4,26 +4,26 @@ App::uses('AppHelper', 'View/Helper');
 class DocumentationHelper extends AppHelper 
 {
     
-	var $helpers = array('Html');
-	
-	var $convertLanguageNotation = array(
-		'eng' => 'en',
-		'spa' => 'es',
-		'fre' => 'fr');
-	
-	public function link()
-	{
-		$userLanguage = Configure::read('Config.language');
-     	$lang         = 'en';
-		if (isset($this->convertLanguageNotation[$userLanguage])) {
-			$lang = $this->convertLanguageNotation[$userLanguage];
-		}
-		
-		echo $this->Html->link(
-	        __('Help'),
-	        'http://vusion-doc.texttochange.org/' . $lang,
-	        array('class' => 'ttc-link-header'));
-	}
-	
-	
+    var $helpers = array('Html');
+    
+    var $convertLanguageNotation = array(
+        'eng' => 'en',
+        'spa' => 'es',
+        'fre' => 'fr');
+    
+    public function link()
+    {
+        $userLanguage = Configure::read('Config.language');
+        $lang         = 'en';
+        if (isset($this->convertLanguageNotation[$userLanguage])) {
+            $lang = $this->convertLanguageNotation[$userLanguage];
+        }
+        
+        echo $this->Html->link(
+            __('Help'),
+            'http://vusion-doc.texttochange.org/' . $lang,
+            array('class' => 'ttc-link-header'));
+    }
+    
+    
 }
