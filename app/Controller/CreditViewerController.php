19fbcbdfb6f88d711bef2455d8801b92bce91bcf
markphilip <markphi119@gmail.com>
2014-08-22 15:56:18 +0300

    added unmatchable reply in csv file

--- app/Controller/CreditViewerController.php
+++ app/Controller/CreditViewerController.php
@@ -130,18 +130,18 @@ class CreditViewerController extends AppController
             $fileFullPath  = $filePath . "/" . $fileName;
             $handle        = fopen($fileFullPath, "w");
             
-            $headers2 = array(
+            $headersDates = array(
                 'Date From',
                 'Date To',
                 '');
             
-            foreach ($headers2 as $header) {
+            foreach ($headersDates as $headerDate) {
                 $line1 = array();
-                if ($header == 'Date From') {
-                    $value = array($header, $timeframeParameters['date-from']);
+                if ($headerDate == 'Date From') {
+                    $value = array($headerDate, $timeframeParameters['date-from']);
                     $line1 = $value;
-                } else if ($header == 'Date To') {
-                    $value = array($header, $timeframeParameters['date-to']);
+                } else if ($headerDate == 'Date To') {
+                    $value = array($headerDate, $timeframeParameters['date-to']);
                     $line1 = $value;
                 } else {
                     $line1[] = '';
@@ -168,7 +168,7 @@ class CreditViewerController extends AppController
             //We extract the data and copy them in the file 
             $creditLogs = $this->_getAllCredits($conditions);
             
-            foreach ($creditLogs as $creditLog) {               
+            foreach ($creditLogs as $creditLog) {
                 foreach ($creditLog['codes'] as $code) {
                     $programsCount = $this->_getProgramCount($code);
                     $programIndex = 0;
@@ -186,6 +186,8 @@ class CreditViewerController extends AppController
                         $programIndex++;
                         fputcsv($handle, $line,',' , '"');
                     }
+                    $unmatchableReply = $this->_getUnmatcahableReplyCount($creditLog, $code, $headers);
+                    fputcsv($handle, $unmatchableReply,',' , '"');
                 }
             }
             
@@ -217,6 +219,27 @@ class CreditViewerController extends AppController
         }
         
         return $programCredits;
+    }
+    
+    
+    protected function _getUnmatcahableReplyCount($creditLog, $code, $headers,  $line = array())
+    {
+        foreach ($headers as $header) {
+            if ($header == 'country') {
+                $line[] = $creditLog['country'];
+            } else if ($header == 'shortcode') {
+                $line[] = $code['code'];
+            } else if ($header == 'program-name' ){
+                $line[] = 'Unmatchable Reply';                                
+            } else if ($header == 'incoming') {
+                $line[] = $code['incoming'];                    
+            } else if ($header == 'outgoing') {
+                $line[] = $code['outgoing'];
+            } else {
+                $line[] = 0;
+            }
+        }
+        return $line;
     }
     
     
--- app/Test/Case/Controller/CreditViewerControllerTest.php
+++ app/Test/Case/Controller/CreditViewerControllerTest.php
@@ -23,7 +23,7 @@ class TestCreditViewerController extends CreditViewerController
 class CreditViewerControllerTestCase extends ControllerTestCase
 {
     public $fixtures = array('app.program', 'app.user', 'app.programsUser', 'app.group');
-      
+    
     
     public function setUp()
     {
@@ -33,12 +33,13 @@ class CreditViewerControllerTestCase extends ControllerTestCase
         $this->Viewer = new TestCreditViewerController();
         $this->Viewer->constructClasses();
         
-        $this->ShortCode = new ShortCode(array('database' => "testdbmongo"));
-        $this->CreditLog = new CreditLog(array('database' => "testdbmongo"));
-        $this->ProgramSettingTest = new ProgramSetting(array('database' => 'testdbprogram'));
-        $this->ProgramSettingM6H = new ProgramSetting(array('database' => 'm6h'));
+        $this->ShortCode           = new ShortCode(array('database' => "testdbmongo"));
+        $this->CreditLog           = new CreditLog(array('database' => "testdbmongo"));
+        $this->ProgramSettingTest  = new ProgramSetting(array('database' => 'testdbprogram'));
+        $this->ProgramSettingM6H   = new ProgramSetting(array('database' => 'm6h'));
         $this->ProgramSettingTrial = new ProgramSetting(array('database' => 'trial'));
-
+        $this->ProgramSettingM9h   = new ProgramSetting(array('database' => 'm9h'));
+        
         $this->dropData();
     }
     
@@ -51,6 +52,7 @@ class CreditViewerControllerTestCase extends ControllerTestCase
         $this->ProgramSettingTest->deleteAll(true, false);  
         $this->ProgramSettingM6H->deleteAll(true, false);
         $this->ProgramSettingTrial->deleteAll(true, false);
+        $this->ProgramSettingM9h->deleteAll(true, false);
     }
     
     
@@ -90,7 +92,7 @@ class CreditViewerControllerTestCase extends ControllerTestCase
         $this->ShortCode->save($shortcode3);
     }
     
-
+    
     public function testIndex_not_timeframed()
     {
         $this->_saveShortcodesInMongoDatabase();
@@ -98,26 +100,26 @@ class CreditViewerControllerTestCase extends ControllerTestCase
         $this->ProgramSettingTest = new ProgramSetting(array('database' => 'testdbprogram'));
         $this->ProgramSettingTest->saveProgramSetting('timezone','Africa/Kampala');
         $this->ProgramSettingTest->saveProgramSetting('shortcode','256-8282');
-
+        
         #One recent creditlog
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', date('Y-m-d'), 'testdbprogram', '256-8282');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog);
-
+        
         #One old creditLog
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', '2012-04-10', 'testdbprogram', '256-8282');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog);
-
+        
         $this->ProgramSettingM6H = new ProgramSetting(array('database' => 'm6h'));
         $this->ProgramSettingM6H->saveProgramSetting('timezone','Africa/Daresalaam');
         $this->ProgramSettingM6H->saveProgramSetting('shortcode','256-8181');
-       
+        
         //not time frame so only show current month
         $this->testAction('/creditViewer/index');
-
+        
         $this->assertTrue(isset($this->vars['countriesCredits']));
         $countriesCredits = $this->vars['countriesCredits'];
         $this->assertEquals(1, count($countriesCredits));
@@ -125,7 +127,7 @@ class CreditViewerControllerTestCase extends ControllerTestCase
         $this->assertEqual('test', $countriesCredits[0]['codes'][0]['programs'][0]['program-name']);
         $this->assertEqual(1, $countriesCredits[0]['codes'][0]['programs'][0]['outgoing']);
     }
-
+    
     
     public function testIndex_timeframed()
     {
@@ -134,43 +136,43 @@ class CreditViewerControllerTestCase extends ControllerTestCase
         $this->ProgramSettingTest = new ProgramSetting(array('database' => 'testdbprogram'));
         $this->ProgramSettingTest->saveProgramSetting('timezone','Africa/Kampala');
         $this->ProgramSettingTest->saveProgramSetting('shortcode','256-8282');
-
+        
         #One recent creditlog
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', '2014-04-10', 'testdbprogram', '256-8282');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog);
-
+        
         #One old creditLog
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', '2013-04-10', 'testdbprogram', '256-8282');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog);
-
+        
         $this->ProgramSettingM6H = new ProgramSetting(array('database' => 'm6h'));
         $this->ProgramSettingM6H->saveProgramSetting('timezone','Africa/Daresalaam');
         $this->ProgramSettingM6H->saveProgramSetting('shortcode','256-8181');
-       
+        
         $this->testAction('/creditViewer/index?date-from=01%2F03%2F2014');
-
+        
         $this->assertTrue(isset($this->vars['countriesCredits']));
         $countriesCredits = $this->vars['countriesCredits'];
         $this->assertEquals(1, count($countriesCredits));
         $this->assertEqual('Uganda', $countriesCredits[0]['country']);
         $this->assertEqual('test', $countriesCredits[0]['codes'][0]['programs'][0]['program-name']);
         $this->assertEqual(1, $countriesCredits[0]['codes'][0]['programs'][0]['outgoing']);
-
+        
         $this->testAction('/creditViewer/index?date-from=01%2F03%2F2013&date-to=01%2F05%2F2013');
-
+        
         $this->assertTrue(isset($this->vars['countriesCredits']));
         $countriesCredits = $this->vars['countriesCredits'];
         $this->assertEquals(1, count($countriesCredits));
         $this->assertEqual('Uganda', $countriesCredits[0]['country']);
         $this->assertEqual('test', $countriesCredits[0]['codes'][0]['programs'][0]['program-name']);
         $this->assertEqual(1, $countriesCredits[0]['codes'][0]['programs'][0]['outgoing']);
-
+        
         $this->testAction('/creditViewer/index?predefined-timeframe=current-month');
-
+        
         $this->assertTrue(isset($this->vars['countriesCredits']));
         $countriesCredits = $this->vars['countriesCredits'];
         $this->assertEquals(0, count($countriesCredits));
@@ -181,37 +183,49 @@ class CreditViewerControllerTestCase extends ControllerTestCase
     {
         $this->_saveShortcodesInMongoDatabase();
         
+        
+        
+        #One recent creditlog
         $this->ProgramSettingTest = new ProgramSetting(array('database' => 'testdbprogram'));
         $this->ProgramSettingTest->saveProgramSetting('timezone','Africa/Kampala');
         $this->ProgramSettingTest->saveProgramSetting('shortcode','256-8282');
-
-        #One recent creditlog
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', date('Y-m-d'), 'testdbprogram', '256-8282');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog);
-        $creditLog = ScriptMaker::mkCreditLog(
-            'program-credit-log', date('Y-m-d'), 'testdbprogram', '254-21222');
-        $this->CreditLog->create();        
-        $this->CreditLog->save($creditLog);
+        
+        $this->ProgramSettingM6H = new ProgramSetting(array('database' => 'm6h'));
+        $this->ProgramSettingM6H->saveProgramSetting('timezone','Africa/Kampala');
+        $this->ProgramSettingM6H->saveProgramSetting('shortcode','256-8282');
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', date('Y-m-d'), 'm6h', '256-8282');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog);
+        
+        $this->ProgramSettingTrial = new ProgramSetting(array('database' => 'trial'));
+        $this->ProgramSettingTrial->saveProgramSetting('timezone','Africa/Kampala');
+        $this->ProgramSettingTrial->saveProgramSetting('shortcode','256-8181');
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', date('Y-m-d'), 'trial', '256-8181');
         $this->CreditLog->create();        
         $this->CreditLog->save($creditLog); 
-
+        
+        $this->ProgramSettingM9h = new ProgramSetting(array('database' => 'm9h'));
+        $this->ProgramSettingM9h->saveProgramSetting('timezone','Africa/Kampala');
+        $this->ProgramSettingM9h->saveProgramSetting('shortcode','254-21222');
+        $creditLog = ScriptMaker::mkCreditLog(
+            'program-credit-log', date('Y-m-d'), 'm9h', '254-21222');
+        $this->CreditLog->create();        
+        $this->CreditLog->save($creditLog);
+        
         #One old creditLog
+        $this->ProgramSettingTest = new ProgramSetting(array('database' => 'testdbprogram'));
+        $this->ProgramSettingTest->saveProgramSetting('timezone','Africa/Kampala');
+        $this->ProgramSettingTest->saveProgramSetting('shortcode','256-8282');
         $creditLog = ScriptMaker::mkCreditLog(
             'program-credit-log', '2012-04-10', 'testdbprogram', '256-8282');
         $this->CreditLog->create();        
-        $this->CreditLog->save($creditLog); 
-
-        $this->ProgramSettingM6H = new ProgramSetting(array('database' => 'm6h'));
-        $this->ProgramSettingM6H->saveProgramSetting('timezone','Africa/Daresalaam');
-        $this->ProgramSettingM6H->saveProgramSetting('shortcode','256-8181');
+        $this->CreditLog->save($creditLog);
         
         $this->testAction("/creditViewer/export?date-from=01%2F03%2F2014&date-to=01%2F05%2F2053");
         
@@ -220,7 +234,7 @@ class CreditViewerControllerTestCase extends ControllerTestCase
             TESTS . 'files/exported_creditViewer.csv',
             WWW_ROOT . 'files/programs/creditViewer/' . $this->vars['fileName']);
         
-       //Asserting that programName "creditViewer" is adding to export file
+        //Asserting that programName "creditViewer" is adding to export file
         $this->assertEquals(
             substr($this->vars['fileName'], 0, -23),
             'creditViewer_');
