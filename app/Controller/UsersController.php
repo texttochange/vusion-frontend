3a1f8b7ae3d366ee503f885e71833622dd37e516
markphilip <markphi119@gmail.com>
2014-09-09 14:53:01 +0300

    changed permission to image folder and changed folder location

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -309,7 +309,7 @@ class UsersController extends AppController
         $userEmail                = $this->Session->read('Auth.User.email');
         $reportIssueToEmail       = Configure::read('vusion.reportIssue.email');
         $reportIssueSubjectPrefix = Configure::read('vusion.reportIssue.subjectPrefix');
-        $filePath                 = WWW_ROOT . 'img';        
+        $filePath                 = WWW_ROOT . 'files/reportIssue';        
         $validationErrors         = array();
         
         if (!isset($this->request->data['ReportIssue']['subject']) || ($this->request->data['ReportIssue']['subject'] == "")) {
