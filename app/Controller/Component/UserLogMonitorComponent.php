33443ffa0b5354b5e49b790a91caf4866fff6989
markphilip <markphi119@gmail.com>
2014-10-15 10:21:00 +0300

    fixed indentation in the component

--- app/Controller/Component/UserLogMonitorComponent.php
+++ app/Controller/Component/UserLogMonitorComponent.php
@@ -6,13 +6,13 @@ App::uses('UserLog', 'Model');
 class UserLogMonitorComponent extends Component
 {
     
-	var $components = array('Session');
-	
-	
-	public function initialize($controller)
-	{
-	    $this->Controller = $controller;
-	    if (!Configure::read("mongo_db")) {
+    var $components = array('Session');
+    
+    
+    public function initialize($controller)
+    {
+        $this->Controller = $controller;
+        if (!Configure::read("mongo_db")) {
             $options = array(
                 'database' => 'vusion'
                 );
@@ -24,101 +24,101 @@ class UserLogMonitorComponent extends Component
         
         $this->UserLog = new UserLog($options);
         
-	    $this->userLogActions  = array(
-	        'default' => array(
-				'POST' => array(
-					'add' => __('Adding to unlisted action in  program.'),
-					'edit' => __('Editing to unlisted action in program.'),
-					'delete' => __('Deleting to unlisted action in program.'))
-				),
-	        'programParticipants' => array(
-	            'POST' => array(
-	                'delete' => __('Deleted participant(s)'),
-	                'add' => __('Added a new participant'),
-	                'edit' => __('Edited participant'),
-	                'import' => __('Imported  participant(s)')
-	                ),
-	            'GET' => array(
-	                'massTag' => __('Tagged participant(s)'),
-	                'massUntag' => __('Untagged participant(s)')
-	                )
-	            ),
-	        'programs' => array(
-	            'POST' => array(
-	                'add' => __('Added a new program'),
-	                'edit' => __('Edited a program'),
-	                'delete' => __('Deleted a program'),
-	                'archive' => __('Archived a program')
-	                )
-	            ),
-	        'programDialogues' => array(
-	            'POST' => array(
-	                'save' => __('Added a new draft dialogue'),
-	                'delete' => __('Deleted a dialogue'),
-	                'activate' => __('Activated a dialogue')
-	                )
-	            ),
-	        'programUnattachedMessages' => array(
-	            'POST' => array(
-	                'add' => __('Added a new separate message'),
-	                'edit' => __('Edited a separate message'),
-	                'delete' => __('Deleted a separate message')
-	                )
-	            ),
-	        'programRequests' => array(
-	            'POST' => array (
-	                'save' => __('Added a new request'),
-	                'delete' => __('Deleted a request')
-	                )
-	            ),
-	        'programHistory' => array(
-	            'POST' => array(
-	                'delete' => __('Deleted program history')
-	                )
-	            ),
-	        'programSettings' => array(
-	            'POST' => array(
-	                'edit' => __('Edited a program setting')
-	                )
-	            ),
-	        'users' => array(
-	            'POST' => array(
-	                'add' => __('Added new a user'),
-	                'edit' => __('Edited a user'),
-	                'delete' => __('Deleted a user'),
-	                'changePassword' => __('Changed password')
-	                )
-	            )
-	        );	    
-	}
-	
-	
-	public function userLogSessionWrite($programDatabaseName = null, $programName = null)
-	{
-	    $controller = 'default';
-		if (isset($this->userLogActions[$this->Controller->request->params['controller']])){
-			$controller = $this->Controller->request->params['controller'];
-		}
-	    
-	    $action = 'index';
-		if (isset($this->Controller->request->action)) {
-			$action = $this->Controller->request->action;
-		}
-		
-		$method = $this->Controller->request->method();
-	    
-	    $this->Session->write('UserLogMonitor', array(
-	        'action' => $action,
-	        'method' => $method,
-	        'controller' => $controller,
-	        'programdatabasename' => $programDatabaseName,
-	        'programname' => $programName));
-	}
-	
-	
-	public function logAction()
-	{
-	    if ($this->Session->check('UserLogMonitor')) {
+        $this->userLogActions  = array(
+            'default' => array(
+                'POST' => array(
+                    'add' => __('Adding to unlisted action in  program.'),
+                    'edit' => __('Editing to unlisted action in program.'),
+                    'delete' => __('Deleting to unlisted action in program.'))
+                ),
+            'programParticipants' => array(
+                'POST' => array(
+                    'delete' => __('Deleted participant(s)'),
+                    'add' => __('Added a new participant'),
+                    'edit' => __('Edited participant'),
+                    'import' => __('Imported  participant(s)')
+                    ),
+                'GET' => array(
+                    'massTag' => __('Tagged participant(s)'),
+                    'massUntag' => __('Untagged participant(s)')
+                    )
+                ),
+            'programs' => array(
+                'POST' => array(
+                    'add' => __('Added a new program'),
+                    'edit' => __('Edited a program'),
+                    'delete' => __('Deleted a program'),
+                    'archive' => __('Archived a program')
+                    )
+                ),
+            'programDialogues' => array(
+                'POST' => array(
+                    'save' => __('Added a new draft dialogue'),
+                    'delete' => __('Deleted a dialogue'),
+                    'activate' => __('Activated a dialogue')
+                    )
+                ),
+            'programUnattachedMessages' => array(
+                'POST' => array(
+                    'add' => __('Added a new separate message'),
+                    'edit' => __('Edited a separate message'),
+                    'delete' => __('Deleted a separate message')
+                    )
+                ),
+            'programRequests' => array(
+                'POST' => array (
+                    'save' => __('Added a new request'),
+                    'delete' => __('Deleted a request')
+                    )
+                ),
+            'programHistory' => array(
+                'POST' => array(
+                    'delete' => __('Deleted program history')
+                    )
+                ),
+            'programSettings' => array(
+                'POST' => array(
+                    'edit' => __('Edited a program setting')
+                    )
+                ),
+            'users' => array(
+                'POST' => array(
+                    'add' => __('Added new a user'),
+                    'edit' => __('Edited a user'),
+                    'delete' => __('Deleted a user'),
+                    'changePassword' => __('Changed password')
+                    )
+                )
+            );        
+    }
+    
+    
+    public function userLogSessionWrite($programDatabaseName = null, $programName = null)
+    {
+        $controller = 'default';
+        if (isset($this->userLogActions[$this->Controller->request->params['controller']])){
+            $controller = $this->Controller->request->params['controller'];
+        }
+        
+        $action = 'index';
+        if (isset($this->Controller->request->action)) {
+            $action = $this->Controller->request->action;
+        }
+        
+        $method = $this->Controller->request->method();
+        
+        $this->Session->write('UserLogMonitor', array(
+            'action' => $action,
+            'method' => $method,
+            'controller' => $controller,
+            'programdatabasename' => $programDatabaseName,
+            'programname' => $programName));
+    }
+    
+    
+    public function logAction()
+    {
+        if ($this->Session->check('UserLogMonitor')) {
             $sessionAction = $this->Session->read('UserLogMonitor');
             $this->_logAction($sessionAction['action'],
                 $sessionAction['method'],
@@ -127,44 +127,44 @@ class UserLogMonitorComponent extends Component
                 $sessionAction['programname']);
             $this->Session->delete('UserLogMonitor');
         }
-	}
-	
-	
-	protected function _logAction($action, $method, $controller, $programDatabaseName = null, $programName = null)
-	{
-	    $now = new DateTime('now');
-	    
-		if (isset($this->Controller->programDetails['database'])) {
-		    $programDatabaseName = $this->Controller->programDetails['database'];
-		} 
-		
-		if (isset($this->Controller->programDetails['name'])) {
-		    $programName = $this->Controller->programDetails['name'];
-		}
-		
-		$programTimezone = 'None';
-		if (isset($this->Controller->programDetails['settings']['timezone'])) {
-		    $programTimezone = $this->Controller->programDetails['settings']['timezone'];
-		}
-		
-		if (!isset($this->userLogActions[$controller][$method][$action])){
-			return true;
-		} else {
-		    $userLog['controller']            = $controller;
-	        $userLog['action']                = $action;
-		    $userLog['program-database-name'] = $programDatabaseName;
-		    $userLog['program-name']          = $programName;
-		    $userLog['parameters']            = $this->userLogActions[$controller][$method][$action];
-		    $userLog['user-id']               = $this->Session->read('Auth.User.id');
-		    $userLog['user-name']             = $this->Session->read('Auth.User.username');
-		    $userLog['timestamp']             = $now->format("d/m/Y H:i:s");
-		    $userLog['timezone']              = $programTimezone;
-		    
-		    $this->UserLog->create();
-		    $this->UserLog->save($userLog);
-		}
-		return true;
-		
-	}
-	
+    }
+    
+    
+    protected function _logAction($action, $method, $controller, $programDatabaseName = null, $programName = null)
+    {
+        $now = new DateTime('now');
+        
+        if (isset($this->Controller->programDetails['database'])) {
+            $programDatabaseName = $this->Controller->programDetails['database'];
+        } 
+        
+        if (isset($this->Controller->programDetails['name'])) {
+            $programName = $this->Controller->programDetails['name'];
+        }
+        
+        $programTimezone = 'None';
+        if (isset($this->Controller->programDetails['settings']['timezone'])) {
+            $programTimezone = $this->Controller->programDetails['settings']['timezone'];
+        }
+        
+        if (!isset($this->userLogActions[$controller][$method][$action])){
+            return true;
+        } else {
+            $userLog['controller']            = $controller;
+            $userLog['action']                = $action;
+            $userLog['program-database-name'] = $programDatabaseName;
+            $userLog['program-name']          = $programName;
+            $userLog['parameters']            = $this->userLogActions[$controller][$method][$action];
+            $userLog['user-id']               = $this->Session->read('Auth.User.id');
+            $userLog['user-name']             = $this->Session->read('Auth.User.username');
+            $userLog['timestamp']             = $now->format("d/m/Y H:i:s");
+            $userLog['timezone']              = $programTimezone;
+            
+            $this->UserLog->create();
+            $this->UserLog->save($userLog);
+        }
+        return true;
+        
+    }
+    
 }
