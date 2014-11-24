77aed3461c8fa3542593fd8b136cf51e61839b93
Olivier Vernin <overnin@gmail.com>
2014-11-24 16:59:15 +0300

    add test for ProgramAuthComponent

--- app/Controller/Component/ProgramAuthComponent.php
+++ app/Controller/Component/ProgramAuthComponent.php
@@ -6,62 +6,60 @@ class ProgramAuthComponent extends Component
 {
 	var $components = array('Auth');
 
+
 	public function startup($controller) 
     {	
-       if ($this->Auth->loggedIn() && !empty($controller->params['program'])) {
-            $controller->Program->recursive = -1;
-            
-            $data = $controller->Program->find('authorized', array(
-                'specific_program_access' => $controller->Group->hasSpecificProgramAccess(
-                    $controller->Auth->user('group_id')),
-                'user_id' => $controller->Auth->user('id'),
-                'program_url' => $controller->params['program']
-                ));
-            if (count($data)==0) {
-                throw new NotFoundException('Could not find this page.');
-            }
+        if (!$this->Auth->loggedIn()) {
+            throw new ForbiddenException();
+        }
+        if (empty($controller->params['program'])) {
+            throw new NotFoundException('Could not find this page.');            
+        }
+        $controller->Program->recursive = -1;
+        
+        $data = $controller->Program->find('authorized', array(
+            'specific_program_access' => $controller->Group->hasSpecificProgramAccess(
+                $this->Auth->user('group_id')),
+            'user_id' => $this->Auth->user('id'),
+            'program_url' => $controller->params['program']
+            ));
+        if (count($data)==0) {
+            throw new NotFoundException('Could not find this page.');
+        }
 
-            $programDetails = array();
-            foreach (array('name', 'url', 'database', 'status') as $key) {
-                $programDetails[$key] = $data[0]['Program'][$key];
-            }
-            $controller->Session->write($programDetails['url']."_db", $programDetails['database']);
-            $controller->Session->write($programDetails['url']."_name", $programDetails['name']);
-            
-            $programSettingModel = ProgramSpecificMongoModel::init('ProgramSetting', $programDetails['database']);
-            $programDetails['settings'] = $programSettingModel->getProgramSettings();
-            $controller->programDetails = $programDetails;
+        $programDetails = array();
+        foreach (array('name', 'url', 'database', 'status') as $key) {
+            $programDetails[$key] = $data[0]['Program'][$key];
+        }
+        $controller->Session->write($programDetails['url']."_db", $programDetails['database']);
+        $controller->Session->write($programDetails['url']."_name", $programDetails['name']);
+        
+        $programSettingModel = ProgramSpecificMongoModel::init('ProgramSetting', $programDetails['database']);
+        $programDetails['settings'] = $programSettingModel->getProgramSettings();
+        $controller->programDetails = $programDetails;
 
-            $controller->_initialize($programDetails['database']);
+        $controller->_initialize($programDetails['database']);
 
-            $controller->set(compact('programDetails'));
-            /*
-            if (!$controller->ArchivedProgram->isAllowed()) {
-                $controller->_stop();
-            }
-            */
-            //In case of a Json request, no need to set up the variables
-            if ($controller->_isAjax() || $controller->params['ext']=='csv') {
-                return;
-            }
-            $currentProgramData = $this->_getCurrentProgramData($programDetails['database']);            
-            $programLogsUpdates = $controller->LogManager->getLogs($programDetails['database'], 5);
-            $programStats       = array('programStats' => $controller->Stats->getProgramStats($programDetails['database'], true));
-            $creditStatus       = $controller->CreditManager->getOverview($programDetails['database']);
-            $controller->set(compact(
-                'currentProgramData',
-                'programLogsUpdates',
-                'programStats',
-                'creditStatus')); 
-        } 
-	}
+        $controller->set(compact('programDetails'));
+        
+        //In case of a Json request, no need to set up the variables
+        if ($controller->_isAjax() || $controller->params['ext']=='csv') {
+            return;
+        }
+        $currentProgramData = $this->_getCurrentProgramData($programDetails['database']);            
+        $programLogsUpdates = $controller->LogManager->getLogs($programDetails['database'], 5);
+        $programStats       = array('programStats' => $controller->Stats->getProgramStats($programDetails['database'], true));
+        $creditStatus       = $controller->CreditManager->getOverview($programDetails['database']);
+        $controller->set(compact(
+            'currentProgramData',
+            'programLogsUpdates',
+            'programStats',
+            'creditStatus')); 
+    }
+
 
 	protected function _getcurrentProgramData($databaseName)
     {
-        /*$unattachedMessageModel = new UnattachedMessage(array('database' => $databaseName));
-        $predefinedMessageModel = new PredefinedMessage(array('database' => $databaseName));        
-        $dialogueModel = new Dialogue(array('database' => $databaseName));
-        $requestModel  = new Request(array('database' => $databaseName));*/
         $unattachedMessageModel = ProgramSpecificMongoModel::init('UnattachedMessage', $databaseName);
         $predefinedMessageModel = ProgramSpecificMongoModel::init('PredefinedMessage', $databaseName);
         $dialogueModel = ProgramSpecificMongoModel::init('Dialogue', $databaseName);
@@ -77,6 +75,7 @@ class ProgramAuthComponent extends Component
             );
         return $currentProgramData;
     }
+
 
 }
 
---
+++ app/Test/Case/Controller/Component/ProgramAuthComponentTest.php
@@ -0,0 +1,196 @@
+<?php
+App::uses('Controller', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('CakeResponse', 'Network');
+App::uses('ComponentCollection', 'Controller');
+App::uses('ProgramAuthComponent', 'Controller/Component');
+
+
+
+class TestProgramAuthComponentController extends Controller
+{
+	var $uses = array(
+		'Program',
+		'Group');
+	var $components = array(
+		'Stats',
+		'LogManager',
+		'CreditManager',
+		'Session');
+	var $programDetails = null;
+
+	function _isAjax() {
+		return false;
+	}
+
+	function _initialize() {
+		return;
+	}
+
+	function redirect($url) {
+		return;
+	}
+
+    function render($view) {
+        return;
+    }
+}
+
+class ProgramAuthComponentTest extends CakeTestCase
+{
+
+	public $fixtures = array(
+		'app.program',
+		'app.user',
+		'app.programsUser',
+		'app.group');
+
+    
+    public function setUp() 
+    {
+        parent::setUp();
+        $Collection                 = new ComponentCollection();
+        $this->ProgramAuthComponent = new ProgramAuthComponent($Collection);
+    }
+    
+    
+    public function tearDown()
+    { 
+        unset($this->ProgramAuthComponent);
+        parent::tearDown();
+    }
+
+    private function _initializeRequest($programUrl, $controllerName="someController",
+    									$method='POST', $action='add', $isAjax=false) {
+
+    	$CakeRequest = $this->getMock('CakeRequest',
+            array('__get', 'method', 'is'));
+
+    	$CakeRequest->action = $action;
+    	$CakeRequest->params = array(
+            'controller' => $controllerName
+            );
+
+        $CakeResponse     = new CakeResponse();
+        $this->Controller = new TestProgramAuthComponentController($CakeRequest, $CakeResponse);
+        $this->Controller->constructClasses();
+      
+        $this->Controller->params['program'] = $programUrl;
+        
+		$this->ProgramAuthComponent->initialize($this->Controller);
+    }
+
+    /**
+     * @expectedException NotFoundException
+     */
+    public function testStartup_programUrl_notDefined()
+    {
+    	$this->_initializeRequest("");
+    	$this->ProgramAuthComponent->Auth = $this->getMock('Auth', array('loggedIn', 'user'));
+    	$this->ProgramAuthComponent->Auth
+    		->expects($this->once())
+    		->method('loggedIn')
+    		->will($this->returnValue(true));
+
+    	$this->ProgramAuthComponent->startup($this->Controller);
+    }
+
+    /**
+     * @expectedException ForbiddenException
+     */
+    public function testStartup_user_notLoggedIn()
+    {
+    	$this->_initializeRequest("");
+    	$this->ProgramAuthComponent->Auth = $this->getMock('Auth', array('loggedIn', 'user'));
+    	$this->ProgramAuthComponent->Auth
+    		->expects($this->once())
+    		->method('loggedIn')
+    		->will($this->returnValue(false));
+
+    	$this->ProgramAuthComponent->startup($this->Controller);
+    }
+
+	/**
+     * @expectedException NotFoundException
+     */
+    public function testStartup_user_notAccess_program()
+    {
+    	$this->_initializeRequest("test");
+    	$this->ProgramAuthComponent->Auth = $this->getMock('Auth', array('loggedIn', 'user'));
+    	$this->ProgramAuthComponent->Auth
+    		->expects($this->once())
+    		->method('loggedIn')
+    		->will($this->returnValue(true));
+
+    	$this->ProgramAuthComponent->Auth
+    		->expects($this->at(1))
+    		->method('user')
+    		->with('group_id')
+    		->will($this->returnValue(2));
+
+   		$this->ProgramAuthComponent->Auth
+    		->expects($this->at(2))
+    		->method('user')
+    		->with('id')
+    		->will($this->returnValue(2));
+
+    	$this->ProgramAuthComponent->startup($this->Controller);
+    }
+
+    public function testStartup_ok()
+    {
+    	$this->_initializeRequest("m6h");
+    	$this->ProgramAuthComponent->Auth = $this->getMock('Auth', array('loggedIn', 'user'));
+    	$this->ProgramAuthComponent->Auth
+    		->expects($this->once())
+    		->method('loggedIn')
+    		->will($this->returnValue(true));
+
+    	$this->ProgramAuthComponent->Auth
+    		->expects($this->at(1))
+    		->method('user')
+    		->with('group_id')
+    		->will($this->returnValue(2));
+
+   		$this->ProgramAuthComponent->Auth
+    		->expects($this->at(2))
+    		->method('user')
+    		->with('id')
+    		->will($this->returnValue(2));
+
+    	$this->Controller->LogManager = $this->getMock('LogManager', array('getLogs'));
+    	$this->Controller->LogManager
+    		->expects($this->once())
+    		->method('getLogs')
+    		->with('m6h')
+    		->will($this->returnValue(array()));
+
+    	$this->Controller->Stats = $this->getMock('Stats', array('getProgramStats'));
+    	$this->Controller->Stats
+    		->expects($this->once())
+    		->method('getProgramStats')
+    		->with('m6h', true)
+    		->will($this->returnValue(array()));
+
+    	$this->Controller->CreditManager = $this->getMock('CreditManager', array('getOverview'));
+    	$this->Controller->CreditManager
+    		->expects($this->once())
+    		->method('getOverview')
+    		->with('m6h')
+    		->will($this->returnValue(array()));
+
+    	$this->ProgramAuthComponent->startup($this->Controller);
+
+    	$this->assertEqual(
+    		array(
+    			'name' => 'm6h',
+		    	'url' => 'm6h',
+		    	'database' => 'm6h',
+		    	'status' => 'running',
+				'settings' => array()), 
+			$this->Controller->programDetails);
+    }
+
+
+}
+
