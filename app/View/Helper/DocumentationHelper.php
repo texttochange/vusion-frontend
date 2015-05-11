a73fc9c9ffdf8d90930f971bd42e5d6c30bd94dd
markphi2013 <markphi119@gmail.com>
2015-05-11 12:52:01 +0300

    removed unused code

--- app/View/Helper/DocumentationHelper.php
+++ app/View/Helper/DocumentationHelper.php
@@ -6,20 +6,10 @@ class DocumentationHelper extends AppHelper
     
     var $helpers = array('Html');
     
-    var $convertLanguageNotation = array(
-        'eng' => 'en',
-        'spa' => 'es',
-        'fre' => 'fr');
     
     public function link()
     {
-        $userLanguage = Configure::read('Config.language');
-        $lang         = 'en';
-        if (isset($this->convertLanguageNotation[$userLanguage])) {
-            $lang = $this->convertLanguageNotation[$userLanguage];
-        }
-       // $documentationUrl = 'http://vusion-doc.texttochange.org/' . $lang;
-       $documentationUrl = 'http://vusion-doc.texttochange.org';
+        $documentationUrl = 'http://vusion-doc.texttochange.org';
         echo $this->Html->link(
             __('Help'),
             array(),
