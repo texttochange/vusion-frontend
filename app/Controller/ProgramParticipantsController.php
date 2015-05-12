f96ad96fdf74df3ef5991686d96bc70d3802d0fa
markphi2013 <markphi119@gmail.com>
2015-05-12 15:58:32 +0300

    added count row number limit to 10,000 participants

--- app/Controller/ProgramParticipantsController.php
+++ app/Controller/ProgramParticipantsController.php
@@ -37,7 +37,7 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         'Paginator' => array(
             'className' => 'BigCountPaginator'));
     
-
+    
     function constructClasses() 
     {
         parent::constructClasses();
@@ -54,7 +54,7 @@ class ProgramParticipantsController extends BaseProgramSpecificController
     {
         parent::beforeFilter();
     }
-
+    
     
     public function index() 
     {      
@@ -64,7 +64,7 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         $requestSuccess = true;
         $order          = null;
         $conditions     = array();
-
+        
         if (isset($this->params['named']['sort']) &&  isset($this->params['named']['direction'])) {
             $order = array($this->params['named']['sort'] => $this->params['named']['direction']);
         }
@@ -72,46 +72,46 @@ class ProgramParticipantsController extends BaseProgramSpecificController
             $this->Participant,
             array(),
             array('Schedule' => $this->Schedule));
-
+        
         $paginate = array(
             'allSafeJoin',
             'conditions' => $conditions,
             'order' => $order);
         $this->paginate = $paginate;
         $participants   = $this->paginate('Participant');
-
+        
         $this->set(compact('participants', 'requestSuccess', 'order'));
     }
     
-
+    
     public function listParticipants()
     {
         $requestSuccess = true;
         $conditions     = array();
         $explodeProfile = array();
-
+        
         if (!$this->_isCsv() && !$this->_isAjax()) {
             throw new MethodNotAllowedException();
         }
-
+        
         $conditions = $this->Filter->getConditions(
             $this->Participant,
             array(),
             array('Schedule' => $this->Schedule));
-
+        
         $participants = $this->Participant->find(
             'allSafeJoin',
             array(
                 'conditions' => $conditions,
                 'limit'=> 10000));
-
+        
         if (isset($this->params['url']['explode_profile'])) {
             $explodeProfile = explode(',', $this->params['url']['explode_profile']);
         }
         $this->set(compact('participants', 'requestSuccess', 'explodeProfile'));
         $this->render('index');
     }
-
+    
     
     protected function _getFilterFieldOptions()
     {   
@@ -232,8 +232,8 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         }
         $this->redirect($redirectUrl);
     }
-
-
+    
+    
     public function exported()
     {
         $programUrl  = $this->programDetails['url'];
@@ -248,7 +248,7 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         $files = $this->paginate('Export');
         $this->set(compact('files'));
     }
-
+    
     
     public function export() 
     {
@@ -269,7 +269,7 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         if (isset($this->params['named']['sort']) &&  isset($this->params['named']['direction'])) {
             $order = array($this->params['named']['sort'] => $this->params['named']['direction']);
         } 
-
+        
         $filePath = WWW_ROOT . "files/programs/" . $programUrl;
         if (!file_exists($filePath)) {
             mkdir($filePath);
@@ -282,10 +282,10 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         }
         $programName  = $this->programDetails['name'];
         $programNameUnderscore = inflector::slug($programName, '_');
-
+        
         $fileName     = $programNameUnderscore . "_participants_" . $timestamp . ".csv";
         $fileFullName = $filePath . DS . $fileName;
-
+        
         $export = array(
             'database' => $this->programDetails['database'],
             'collection' => $this->Participant->table,
@@ -309,11 +309,11 @@ class ProgramParticipantsController extends BaseProgramSpecificController
             'action' => 'exported'));
     }
     
-
+    
     public function deleteExport() 
     {
         $id = $this->params['id'];
-
+        
         if (!$this->request->is('post')) {
             throw new MethodNotAllowedException();
         }
@@ -321,19 +321,19 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         if (!$this->Export->exists()) {
             throw new NotFoundException(__('Invalid Export: %s', $id));
         }
-
+        
         if ($this->Export->delete()) {
             $this->Session->setFlash(__('Export deleted.'),
                 'default', array('class'=>'message success'));
         } else {
             $this->Session->setFlash(__('Export cannot be deleted.'));
         }
-
+        
         $this->redirect(array(
             'program' => $this->programDetails['url'],
             'action' => 'exported'));
     }
-
+    
     
     protected function _searchProfile($array, $labelKey)
     {
@@ -365,18 +365,18 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         $this->VumiRabbitMQ->sendMessageMassUntag($workerName, $tag);
     }
     
-
+    
     protected function _notifyBackendRunActions($workerName, $runActions)
     {
         $this->VumiRabbitMQ->sendMessageRunActions($workerName, $runActions);
     }
-
-
+    
+    
     protected function _notifyBackendExport($exportId)
     {
         $this->VumiRabbitMQ->sendMessageToExport($exportId);
     }
-
+    
     
     public function add() 
     {
@@ -448,8 +448,8 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         }
         return $participant;
     }
-
-   
+    
+    
     protected function _ajaxDataPatch($modelName='Participant')
     {
         $data = $this->data;
@@ -458,8 +458,8 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         }
         return $data;
     }
-
-
+    
+    
     //we should not be able to edit a phone number
     public function edit()   
     {
@@ -751,91 +751,107 @@ class ProgramParticipantsController extends BaseProgramSpecificController
     
     public function import()
     {
-    	    $programName = $this->Session->read($this->params['program'].'_name');
-    	    $programUrl  = $this->params['program'];
-    	    
-    	    if ($this->request->is('post')) {
-    	    	    if (!$this->ProgramSetting->hasRequired()) {
-    	    	    	    $this->Session->setFlash(
-    	    	    	    	    __('Please set the program settings then try again.'), 
-    	    	    	    	    'default', array('class' => "message failure")
-    	    	    	    	    );
-    	    	    	    return;
-    	    	    }
-    	    	    
-    	    	    if ($this->request->data['Import']['file']['error'] != 0) {
-    	    	    	    if ($this->request->data['Import']['file']['error'] == 4) { 
-    	    	    	    	    $message = __("Please select a file.");
-    	    	    	    } else if ($this->request->data['Import']['file']['error'] == 1) {
-    	    	    	    	    $message = __("Selected file is too large to import please divide it into 15,000 participants.");
-    	    	    	    } else { 
-    	    	    	    	    $message = __('Error while uploading the file: %s.', $this->request->data['Import']['file']['error']);
-    	    	    	    }
-    	    	    	    $this->Session->setFlash($message, 
-    	    	    	    	    'default', array('class' => "message failure")
-    	    	    	    	    );
-    	    	    	    return;
-    	    	    }
-    	    	    
-    	    	    
-    	    	    $tags = null;
-    	    	    if (isset($this->request->data['Import']['tags'])) {
-    	    	    	    $tags = $this->request->data['Import']['tags'];
-    	    	    }
-    	    	    
-    	    	    $replaceTagsAndLabels = false;
-    	    	    if (isset($this->request->data['Import']['replace-tags-and-labels'])) {
-    	    	    	    $replaceTagsAndLabels = true;
-    	    	    }
-    	    	    
-    	    	    $fileName = $this->request->data['Import']['file']['name'];
-    	    	    
-    	    	    $filePath = WWW_ROOT . "files/programs/" . $programUrl; 
-    	    	    
-    	    	    if (!file_exists(WWW_ROOT . "files/programs/".$programUrl)) {
-    	    	    	    mkdir($filePath);
-    	    	    	    chmod($filePath, 0764);
-    	    	    }
-    	    	    
-    	    	    /** in case the file has already been created, 
-    	    	    * the chmod function should not be called.
-    	    	    */
-    	    	    $wasFileAlreadyThere = false;
-    	    	    if (file_exists($filePath . DS . $fileName)) {
-    	    	    	    $wasFileAlreadyThere = true;
-    	    	    }
-    	    	    
-    	    	    copy($this->request->data['Import']['file']['tmp_name'],
-    	    	    	    $filePath . DS . $fileName);
-    	    	    
-    	    	    if (!$wasFileAlreadyThere) {
-    	    	    	    chmod($filePath . DS . $fileName, 0664);
-    	    	    }
-    	    	    
-    	    	    $report = $this->Participant->import(
-    	    	    	    $programUrl, 
-    	    	    	    $filePath . DS . $fileName, 
-    	    	    	    $tags,
-    	    	    	    $replaceTagsAndLabels
-    	    	    	    );
-    	    	    if ($report) {
-    	    	    	    foreach ($report as $participantReport) {
-    	    	    	    	    if ($participantReport['saved']) {
-    	    	    	    	    	    $this->_notifyUpdateBackendWorker($programUrl, $participantReport['phone']);
-    	    	    	    	    }    
-    	    	    	    }
-    	    	    	    $requestSuccess = true;
-    	    	    } else {
-    	    	    	    $this->Session->setFlash(
-    	    	    	    	    $this->Participant->importErrors[0], 
-    	    	    	    	    'default', array('class' => "message failure")
-    	    	    	    	    );
-    	    	    }
-    	    	    throw new Exception();
-    	    	    //Remove file at the end of the import
-    	    	    unlink($filePath . DS . $fileName);
-    	    }
-    	    $this->set(compact('report', 'requestSuccess'));
+        $programName = $this->Session->read($this->params['program'].'_name');
+        $programUrl  = $this->params['program'];
+        
+        if ($this->request->is('post')) {
+            if (!$this->ProgramSetting->hasRequired()) {
+                $this->Session->setFlash(
+                    __('Please set the program settings then try again.'), 
+                    'default', array('class' => "message failure")
+                    );
+                return;
+            }
+            
+            
+            if ($this->request->data['Import']['file']['error'] != 0) {
+                if ($this->request->data['Import']['file']['error'] == 4) { 
+                    $message = __("Please select a file.");
+                } else if ($this->request->data['Import']['file']['error'] == 1) {
+                    $message = __("Selected file is too large to import please divide it into 15,000 participants.");
+                } else { 
+                    $message = __('Error while uploading the file: %s.', $this->request->data['Import']['file']['error']);
+                }
+                $this->Session->setFlash($message, 
+                    'default', array('class' => "message failure")
+                    );
+                return;
+            }
+            
+            $file      = $this->request->data['Import']['file']['tmp_name'];
+            $linecount = 0;
+            $handle    = fopen($file, "r");
+            while(!feof($handle)){
+                $line = fgets($handle);
+                $linecount++;
+            }
+            fclose($handle);
+            
+            if ($linecount > 10000) {
+                $this->Session->setFlash(__('File exceeds 10,000 pranticipants'), 
+                    'default', array('class' => "message failure")
+                    );
+                return;
+            }
+            
+            $tags = null;
+            if (isset($this->request->data['Import']['tags'])) {
+                $tags = $this->request->data['Import']['tags'];
+            }
+            
+            $replaceTagsAndLabels = false;
+            if (isset($this->request->data['Import']['replace-tags-and-labels'])) {
+                $replaceTagsAndLabels = true;
+            }
+            
+            $fileName = $this->request->data['Import']['file']['name'];
+            
+            $filePath = WWW_ROOT . "files/programs/" . $programUrl; 
+            
+            if (!file_exists(WWW_ROOT . "files/programs/".$programUrl)) {
+                mkdir($filePath);
+                chmod($filePath, 0764);
+            }
+            
+            /** in case the file has already been created, 
+            * the chmod function should not be called.
+            */
+            $wasFileAlreadyThere = false;
+            if (file_exists($filePath . DS . $fileName)) {
+                $wasFileAlreadyThere = true;
+            }
+            
+            copy($this->request->data['Import']['file']['tmp_name'],
+                $filePath . DS . $fileName);
+            
+            if (!$wasFileAlreadyThere) {
+                chmod($filePath . DS . $fileName, 0664);
+            }
+            
+            $report = $this->Participant->import(
+                $programUrl, 
+                $filePath . DS . $fileName, 
+                $tags,
+                $replaceTagsAndLabels
+                );
+            if ($report) {
+                foreach ($report as $participantReport) {
+                    if ($participantReport['saved']) {
+                        $this->_notifyUpdateBackendWorker($programUrl, $participantReport['phone']);
+                    }    
+                }
+                $requestSuccess = true;
+            } else {
+                $this->Session->setFlash(
+                    $this->Participant->importErrors[0], 
+                    'default', array('class' => "message failure")
+                    );
+            }
+            // throw new Exception();
+            //Remove file at the end of the import
+            unlink($filePath . DS . $fileName);
+        }
+        $this->set(compact('report', 'requestSuccess'));
     }
     
     
@@ -849,18 +865,18 @@ class ProgramParticipantsController extends BaseProgramSpecificController
         $paginationCount   = $this->Participant->count($this->Filter->getConditions($this->Participant, $defaultConditions), null, -1);
         $this->set(compact('requestSuccess', 'paginationCount'));
     }
-
-
+    
+    
     public function runActions()
     {
         $programUrl     = $this->params['program'];
         $requestSuccess = true;
         $data           = $this->data;
-
+        
         if (!$this->_isAjax()) {
             throw new MethodNotAllowedException();
         }
-
+        
         $valid = $this->Participant->validateRunActions($data);
         if ($valid === true) {
             $this->Session->setFlash(__('The runActions succeed.'));
