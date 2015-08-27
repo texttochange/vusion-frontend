212d6b80bde83e4ca7896775f290cfb7f55684d5
markphi2013 <markphi119@gmail.com>
2015-08-27 10:12:05 +0300

    added genenrate html functions in Js

--- app/webroot/js/ttc-simulator.js
+++ app/webroot/js/ttc-simulator.js
@@ -39,86 +39,86 @@
             for (var i = 0; i< history.length; i++) {
                 message = history[i]['History'];
                 if (message['message-direction'] == "incoming") {
-                    el.append(
-                        "<div class='simulator-msg'><div class='simulator-incoming'> <div>"+
-                        message['message-content']+
-                        "</div><div class='simulator-datetime'>"+
-                        moment(message['timestamp']).calendar()+
-                        "</div></div></div>")
+                    el.append(generateHtmlHistoryMessage(message))
                     container[0].scrollTop = container[0].scrollHeight;
                 } else if (message['message-direction'] == "outgoing") {
-                    el.append(
-                        "<div class='simulator-msg'><div class='simulator-outgoing'> <div>"+                                
-                        message['message-content']+
-                        "</div><div class='simulator-datetime'>"+
-                        moment(message['timestamp']).calendar()+
-                        "</div></div></div>")
+                    el.append(generateHtmlHistoryMessage(message))
                     container[0].scrollTop = container[0].scrollHeight;
                 } else {
                     $("#simulator-output").append("")
                 }
             }
 		}
-
+		
 		function processParticipant(participant) {
-			$('#simulator-profile').empty()
-            $("#simulator-profile").append(
-                "<dl> <dt>"+
-                "Phone: "+
-                participant['phone']+
-                "</dt></dl>")
-            $("#simulator-profile").append(
-                "<dl> <dt>"+
-                "Last Optin Date:  </br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+
-                 moment(participant['last-optin-date']).format("DD/MM/YYYY HH:mm:ss")+
-                "</dt></dl>")
-            $("#simulator-profile").append(
-                    "<dl> <dt>"+
-                    "Last Optout Date: </br>")
-            if (participant['last-optout-date']) {
-                $("#simulator-profile").append(
-                    "<dl><dt>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+
-                     moment(participant['last-optout-date']).format("DD/MM/YYYY HH:mm:ss")+
-                    "</dt></dl>")
-            } else {
-                $("#simulator-profile").append("&nbsp;  </dt></dl>")
-            }
-            
-            if ((participant['profile'].length) > 0) {
-                $("#simulator-profile").append(
-                    "<dl><dt>"+
-                    "Labels: ")
-                for (var i = 0; i < participant['profile'].length; i++) {
-                    $("#simulator-profile").append(
-                        "<dl><dt>"+
-                        "<div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+
-                        participant['profile'][i]['label']+
-                        ": "+
-                        participant['profile'][i]['value']+
-                        "</div></dt></dl>"
-                        )
-                }
-            } else {
-                $("#simulator-profile").append("&nbsp;  </dt></dl>")
-            }
-            
-            if ((participant['tags'].length) > 0) {
-                $("#simulator-profile").append(
-                    "<dl><dt>"+
-                    "Tags: ")
-                for (var i = 0; i < participant['tags'].length; i++) {
-                    $("#simulator-profile").append(
-                        "<dl><dt>"+
-                        "<div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+
-                        participant['tags'][i]+
-                        "</div></dt></dl>"
-                        )
-                }
-            } else {
-                $("#simulator-profile").append("&nbsp;  </dt></dl>")
-            }
+		    $('#simulator-profile').empty()
+		    $("#simulator-profile").append(generateHtmlParticipant(participant));
 		}
-
+		
+		function generateHtmlHistoryMessage(message) {
+		    var myTemplate = "<div class='simulator-msg'>"+
+		    "<div" +((message['message-direction'] == 'incoming') ? " class='simulator-incoming'" : " class='simulator-outgoing'" )+">"+
+		    "<div>"+
+		    "MESSAGE_CONTENT"+
+		    "</div>"+
+		    "<div class='simulator-datetime'>"+
+		    "MESSAGE_TIMESTAMP"+
+		    "</div>"+
+		    "</div>"+
+		    "</div>"
+		    
+		    myTemplate = myTemplate.replace('MESSAGE_CONTENT', message['message-content']);
+		    myTemplate = myTemplate.replace('MESSAGE_TIMESTAMP', moment(message['timestamp']).calendar());
+		    return  myTemplate;
+		}
+		
+		function generateHtmlParticipant(participant) {
+		    var myTemplate = "<dl> <dt>"+
+		    "Phone: "+
+		    "PARTICIPANT_PHONE"+
+		    "</dt></dl>"+
+		    "<dl><dt>"+
+		    "Last Optin Date:</br>"+
+		    "PARTICIPANT_OPTIN_DATE"+
+		    "</dt></dl>"+
+		    "<dl><dt>"+
+            "Last Optout Date: </br>"+
+		    ((participant['last-optout-date']) ? moment(participant['last-optout-date']).format("DD/MM/YYYY HH:mm:ss")+
+		        "</dt></dl>" :  "&nbsp;  </dt></dl>" )+
+		    (((participant['profile'].length) > 0) ? "<dl><dt>"+
+                    "Labels:<div id='simulator-labels'> PARTICIPANT_LABELS </div>" : "&nbsp;  </dt></dl>")+
+            (((participant['tags'].length) > 0) ? "<dl><dt>"+
+                    "Tags: <div id='simulator-tags'> PARTICIPANT_TAGS </div>": "&nbsp;  </dt></dl>")
+            
+            
+		    myTemplate = myTemplate.replace('PARTICIPANT_PHONE', participant['phone']);
+		    myTemplate = myTemplate.replace('PARTICIPANT_OPTIN_DATE', moment(participant['last-optin-date']).format("DD/MM/YYYY HH:mm:ss"));
+            myTemplate = myTemplate.replace('PARTICIPANT_LABELS', generateHtmlParticipantLabels(participant));
+            myTemplate = myTemplate.replace('PARTICIPANT_TAGS', generateHtmlParticipantTags(participant));
+		    return  myTemplate;
+            
+            function generateHtmlParticipantLabels(participant) {
+                for (var i = 0; i < participant['profile'].length; i++) {
+                   $("#simulator-labels").append("<div class='simulator-profile-value'>"+
+                    participant['profile'][i]['label']+
+                    ": "+
+                    participant['profile'][i]['value']+
+                    "</div></dt></dl>")
+                }
+                
+            }
+            
+            function generateHtmlParticipantTags(participant) {
+                for (var i = 0; i < participant['tags'].length; i++) {
+                    $("#simulator-tags").append("<div class='simulator-profile-value'>"+
+                    participant['tags'][i]+
+                    "</div></dt></dl>")
+                }
+            }
+            
+		}
+		
+		
 
 		// initial pull
 		$.ajax({
@@ -148,7 +148,7 @@
 			});
 		};
 
-	    setInterval(el.update, 3000);
+	    setInterval(el.update, 300000);
 
 	    return el;
 	}
