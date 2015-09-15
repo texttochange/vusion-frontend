a3b172666673d15ede28678c8915d1132b1d9286
markphi2013 <markphi119@gmail.com>
2015-09-15 13:18:22 +0300

    changed to while loop on generate simulatedphone

--- app/Model/Participant.php
+++ app/Model/Participant.php
@@ -399,14 +399,16 @@ class Participant extends ProgramSpecificMongoModel
     
     
     public function generateSimulatedPhone()
-    { 
-        for ($i=1; $i<$i+1; $i++) {
+    {  
+        $i=1;
+        while ($i<$i+1) {
             $simulatedPhone = ("#" . $i );
             $result = $this->find('count', array(
                 'conditions' => array('phone' => $simulatedPhone)));
             if ($result < 1) {
                 return $simulatedPhone;
             }
+            $i++;
         }
     }
         
