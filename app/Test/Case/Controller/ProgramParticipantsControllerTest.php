ad5eec2d53bb8f4b7ec4d2b565fe7d6f00979b7d
markphi2013 <markphi119@gmail.com>
2015-09-16 17:50:09 +0300

    added \r

--- app/Test/Case/Controller/ProgramParticipantsControllerTest.php
+++ app/Test/Case/Controller/ProgramParticipantsControllerTest.php
@@ -1611,8 +1611,7 @@ class ProgramParticipantsControllerTestCase extends ControllerTestCase
                 'method' => 'post',
                 'data' => array(
                     'phone' => '#6',
-                    'message' => "  testing send
-                      ",
+                    'message' => "  testing send\r ",
                     )
                 )
             );
