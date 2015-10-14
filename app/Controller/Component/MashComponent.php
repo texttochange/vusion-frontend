eee5e020e45ff9d14d8e59b006bcc35d02a5171f
Olivier Vernin <overnin@gmail.com>
2015-10-14 23:20:00 +0200

    fix mash import

--- app/Controller/Component/MashComponent.php
+++ app/Controller/Component/MashComponent.php
@@ -5,22 +5,23 @@ App::uses('BaseProgramSpecificController', 'Controller');
 class MashComponent extends Component
 {
 
-	public function initialize(BaseProgramSpecificController $controller)
-	{
-		$this->controller = $controller;
-	}
+    public function initialize(BaseProgramSpecificController $controller)
+    {
+        $this->controller = $controller;
+    }
 
-	public function importParticipants($country) {
-		$importMaxParticipants = Configure::read('vusion.importMaxParticipants');
+    public function importParticipants($country) {
+        $importMaxParticipants = Configure::read('vusion.importMaxParticipants');
         require_once('mash-api/lib/MashApi.php');
         $api = new MashApi(
             Configure::read('vusion.mash.apiKey'),
             Configure::read('vusion.mash.url'));
-        $participants = $api->getParticipants(
+        $participants = $api->importParticipants(
             $country,
             $this->controller->programDetails['url'],
             $importMaxParticipants);
+        print_r($participants);
         return $participants;
-	}
+    }
 
 }
