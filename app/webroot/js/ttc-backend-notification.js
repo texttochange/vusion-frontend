1a8816ed057ffa25fd7b9a191c23779f4ca3fa3d
Olivier Vernin <overnin@gmail.com>
2015-10-21 12:32:51 +0200

    fix pulling

--- app/webroot/js/ttc-backend-notification.js
+++ app/webroot/js/ttc-backend-notification.js
@@ -1,71 +1,70 @@
 (function()
-    {
-        
-        $.fn.extend({
-                pullBackend: function(url) {
-                    if (typeof $(this).data('pullBackend') === 'undefined') {
-                        $(this).data('pullBackend', pullBackendNotifications($(this), url));
-                    }
-                }
-        });
-        
-        
-        function pullBackendNotifications(el, url) {
-            var myFuncCalls = 0;
-            function processError(jqXHR, textStatus, errorThrown) {
-                myFuncCalls++;
-                if (errorThrown == 'Forbidden') {
-                    var refreshRate = 2000;
-                    while (true) {
-                        refreshRate *= myFuncCalls;
-                        if (this.reschedule) {
-                            return setTimeout(function(){el.update(true);}, refreshRate);                            
-                        }
-                        
-                    }
-                }
-                vusionAjaxError(jqXHR, textStatus, errorThrown);
-            }
-            
-            function processResponse(data) {
-                $('#connectionState').hide();
-                startPulling(data);
-                if (this.reschedule) {
-                    setTimeout(function(){el.update(true);}, 10000);
-                }
-            } 
-            
-            
-            function startPulling(data) {               
-                if (data['logs']) {
-                    $("#notifications").empty();
-                    for (var x = 0; x < data['logs'].length; x++) {
-                        data['logs'][x] = data['logs'][x].replace(data['logs'][x].substr(1,19),"<span style='font-weight:bold'>"+data['logs'][x].substr(1,19)+"</span>");
-                        $("#notifications").append(data['logs'][x]+"<br \>");
-                    }
+{
+    
+    $.fn.extend({
+            pullBackend: function(url) {
+                if (typeof $(this).data('pullBackend') === 'undefined') {
+                    $(this).data('pullBackend', pullBackendNotifications($(this), url));
                 }
             }
-            
-            
+    });
+    
+    
+    function pullBackendNotifications(el, url) {
+        var numberOfConsecutiveErrors = 0,
+            normalRefreshRate = 20000, //pull every 20seconds if everything is ok
+            currentRefreshRate = normalRefreshRate;  //initialy the refresh rate is ok
+
+        function processError(jqXHR, textStatus, errorThrown) {
+            numberOfConsecutiveErrors++;
+            //if (errorThrown == 'Forbidden') {  //let's slow down in all error case
+            //var refreshRate = 2000;
+            currentRefreshRate = currentRefreshRate * numberOfConsecutiveErrors;  //update the current refresh rate
+            setTimeout(function(){el.update();}, currentRefreshRate);            
+            //}
+            vusionAjaxError(jqXHR, textStatus, errorThrown);
+        }
+        
+        function processResponse(data) {
+            numberOfConsecutiveErrors = 0;
+            currentRefreshRate = normalRefreshRate;
+            $('#connectionState').hide();
+            startPulling(data);   //name not appropriate more like "displayLogs"
+            setTimeout(function(){el.update();}, currentRefreshRate);
+        } 
+        
+        
+        function startPulling(data) {               
+            if (data['logs']) {
+                $("#notifications").empty();
+                for (var x = 0; x < data['logs'].length; x++) {
+                    data['logs'][x] = data['logs'][x].replace(data['logs'][x].substr(1,19),"<span style='font-weight:bold'>"+data['logs'][x].substr(1,19)+"</span>");
+                    $("#notifications").append(data['logs'][x]+"<br \>");
+                }
+            }   // and else what do javascript should do?
+        }
+        
+        
+        /*$.ajax({ 
+                url: url,
+                type: 'GET',
+                reschedule: true,
+                success: processResponse,
+                timeout: 500,     
+                error: processError,
+        });*/
+        
+        el.update = function(reschedule) {
             $.ajax({ 
                     url: url,
                     type: 'GET',
-                    reschedule: true,
                     success: processResponse,
                     timeout: 500,
                     error: processError,
             });
-            
-            el.update = function(reschedule) {
-                $.ajax({ 
-                        url: url,
-                        type: 'GET',
-                        reschedule: true,
-                        success: processResponse,
-                        timeout: 500,
-                        error: processError,
-                });
-            };
-        }
-        
-    })(jQuery);
+        };
+
+        el.update();  //initial call
+    }
+    
+})();
