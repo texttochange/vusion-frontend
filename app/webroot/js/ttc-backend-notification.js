7e6426a7fac08a677a66dce6256b95386525753b
markphi2013 <markphi119@gmail.com>
2015-10-21 14:27:45 +0300

    removed comments

--- app/webroot/js/ttc-backend-notification.js
+++ app/webroot/js/ttc-backend-notification.js
@@ -1,70 +1,60 @@
 (function()
-{
-    
-    $.fn.extend({
-            pullBackend: function(url) {
-                if (typeof $(this).data('pullBackend') === 'undefined') {
-                    $(this).data('pullBackend', pullBackendNotifications($(this), url));
-                }
-            }
-    });
-    
-    
-    function pullBackendNotifications(el, url) {
-        var numberOfConsecutiveErrors = 0,
-            normalRefreshRate = 20000, //pull every 20seconds if everything is ok
-            currentRefreshRate = normalRefreshRate;  //initialy the refresh rate is ok
-
-        function processError(jqXHR, textStatus, errorThrown) {
-            numberOfConsecutiveErrors++;
-            //if (errorThrown == 'Forbidden') {  //let's slow down in all error case
-            //var refreshRate = 2000;
-            currentRefreshRate = currentRefreshRate * numberOfConsecutiveErrors;  //update the current refresh rate
-            setTimeout(function(){el.update();}, currentRefreshRate);            
-            //}
-            vusionAjaxError(jqXHR, textStatus, errorThrown);
-        }
+    {
         
-        function processResponse(data) {
-            numberOfConsecutiveErrors = 0;
+        $.fn.extend({
+                pullBackend: function(url) {
+                    if (typeof $(this).data('pullBackend') === 'undefined') {
+                        $(this).data('pullBackend', pullBackendNotifications($(this), url));
+                    }
+                }
+        });
+        
+        
+        function pullBackendNotifications(el, url) {
+            var numberOfConsecutiveErrors = 0,
+            normalRefreshRate = 20000, 
             currentRefreshRate = normalRefreshRate;
-            $('#connectionState').hide();
-            startPulling(data);   //name not appropriate more like "displayLogs"
-            setTimeout(function(){el.update();}, currentRefreshRate);
-        } 
-        
-        
-        function startPulling(data) {               
-            if (data['logs']) {
-                $("#notifications").empty();
-                for (var x = 0; x < data['logs'].length; x++) {
-                    data['logs'][x] = data['logs'][x].replace(data['logs'][x].substr(1,19),"<span style='font-weight:bold'>"+data['logs'][x].substr(1,19)+"</span>");
-                    $("#notifications").append(data['logs'][x]+"<br \>");
+            
+            function processError(jqXHR, textStatus, errorThrown) {
+                numberOfConsecutiveErrors++;
+                //update the current refresh rate
+                currentRefreshRate = currentRefreshRate * numberOfConsecutiveErrors;  
+                setTimeout(function(){el.update();}, currentRefreshRate);            
+                vusionAjaxError(jqXHR, textStatus, errorThrown);
+            }
+            
+            function processResponse(data) {
+                numberOfConsecutiveErrors = 0;
+                currentRefreshRate = normalRefreshRate;
+                $('#connectionState').hide();
+                displayLogs(data);
+                setTimeout(function(){el.update();}, currentRefreshRate);
+            } 
+            
+            
+            function displayLogs(data) {               
+                if (data['logs']) {
+                    $("#notifications").empty();
+                    for (var x = 0; x < data['logs'].length; x++) {
+                        data['logs'][x] = data['logs'][x].replace(data['logs'][x].substr(1,19),"<span style='font-weight:bold'>"+data['logs'][x].substr(1,19)+"</span>");
+                        $("#notifications").append(data['logs'][x]+"<br \>");
+                    }
                 }
-            }   // and else what do javascript should do?
+                return;
+            }
+            
+            
+            el.update = function() {
+                $.ajax({ 
+                        url: url,
+                        type: 'GET',
+                        success: processResponse,
+                        timeout: 500,
+                        error: processError,
+                });
+            };
+            
+            el.update();  //initial call
         }
         
-        
-        /*$.ajax({ 
-                url: url,
-                type: 'GET',
-                reschedule: true,
-                success: processResponse,
-                timeout: 500,     
-                error: processError,
-        });*/
-        
-        el.update = function(reschedule) {
-            $.ajax({ 
-                    url: url,
-                    type: 'GET',
-                    success: processResponse,
-                    timeout: 500,
-                    error: processError,
-            });
-        };
-
-        el.update();  //initial call
-    }
-    
-})();
+    })();
